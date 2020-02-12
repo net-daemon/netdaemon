d02fc0be4c59897768203442dedfa44a6a23bf15
helto4real <tomas.hellstrom@yahoo.se>
2020-02-12 21:09:50 +0100

    Added possibility to config loglevel  hass add-on

--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -1,14 +1,50 @@
-﻿using JoySoftware.HomeAssistant.NetDaemon.DaemonRunner.Service;
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using JoySoftware.HomeAssistant.NetDaemon.DaemonRunner.Service;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Service
 {
+
     class Program
     {
-        public static void Main(string[] args)
+        private const string _hassioConfigPath = "data/options.json";
+        private static LogLevel LogLevel = LogLevel.Information;
+        public static async Task Main(string[] args)
         {
+
+            try
+            {
+                ///
+                if (File.Exists(_hassioConfigPath))
+                {
+
+                    var hassAddOnSettings = await JsonSerializer.DeserializeAsync<System.Collections.Generic.Dictionary<string, string>>(
+                                                        File.OpenRead(_hassioConfigPath));
+                    if (hassAddOnSettings.ContainsKey("log_level"))
+                    {
+                        Program.LogLevel = hassAddOnSettings["log_level"] switch
+                        {
+                            "information" => LogLevel.Information,
+                            "debug" => LogLevel.Debug,
+                            "error" => LogLevel.Error,
+                            "warning" => LogLevel.Warning,
+                            "trace" => LogLevel.Trace,
+                            _ => LogLevel.Information
+                        };
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("Home assistant add-on config not valid json, ending add-on...");
+                return;
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -18,10 +54,10 @@ namespace Service
                 .ConfigureLogging(logging =>
                 {
                     logging.ClearProviders();
-                    logging.AddConsole(options => options.IncludeScopes=false);
+                    logging.AddConsole(options => options.IncludeScopes = false);
                     logging.AddDebug();
                     logging.AddFilter("Microsoft", LogLevel.Error);
-                    logging.SetMinimumLevel(LogLevel.Information);
+                    logging.SetMinimumLevel(Program.LogLevel);
                 });
     }
 }
