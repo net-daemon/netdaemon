580d37a7038021907ca883927bf86cad24b0b11f
John Vert <john@groopit.co>
2020-05-08 20:47:42 +0200

    temporarily disable this check for Execute()

--- src/DaemonRunner/DaemonRunner/Service/App/CodeManager.cs
+++ src/DaemonRunner/DaemonRunner/Service/App/CodeManager.cs
@@ -636,8 +636,8 @@ namespace JoySoftware.HomeAssistant.NetDaemon.DaemonRunner.Service.App
                         var x = syntaxTree.GetLineSpan(topInvocationExpression.Span);
                         if (linesReported.Contains(x.StartLinePosition.Line) == false)
                         {
-                            _logger.LogError($"Missing Execute or ExecuteAsync in {syntaxTree.FilePath} ({x.StartLinePosition.Line + 1},{x.StartLinePosition.Character + 1}) near {topInvocationExpression.ToFullString().Trim()}");
-                            linesReported.Add(x.StartLinePosition.Line);
+                            // _logger.LogError($"Missing Execute or ExecuteAsync in {syntaxTree.FilePath} ({x.StartLinePosition.Line + 1},{x.StartLinePosition.Character + 1}) near {topInvocationExpression.ToFullString().Trim()}");
+                            // linesReported.Add(x.StartLinePosition.Line);
                         }
                     }
                 }
