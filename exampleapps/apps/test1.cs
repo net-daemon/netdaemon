29dc2fb56dc2f5c9b7ce12e616273cb51db7be58
John Vert <john@groopit.co>
2020-05-08 20:47:59 +0200

    add more little tests

--- exampleapps/apps/test1.cs
+++ exampleapps/apps/test1.cs
@@ -13,12 +13,24 @@ public class GlobalApp : NetDaemonApp
 
     public override async Task InitializeAsync()
     {
-        Log("Listening for events with Rx");
-        this.ForEach(e =>
+
+        Log("Listening for all events with Rx");
+        this.Subscribe(e =>
         {
-            Log("Rx - got event for {0}, state is {1}", e.EntityId, e.State);
+            Log("Rx - got global event for {0}, state is {1}", e.EntityId, e.State);
         });
 
+        Log("listening for specific entity events with Rx");
+        Entity("light.stairs").Subscribe(e =>
+        {
+            Log("Rx - got specific light.stairs event for {0}: {1}", e.EntityId, e.State);
+        });
+
+        Log("listing for 2 entity events with Rx");
+        Entity("light.stairs", "light.dining_light").Subscribe(e =>
+        {
+            Log("Rx - got either stairs or dining event for {0}: {1}", e.EntityId, e.State);
+        });
     }
 }
 
--- src/CustomApp/MyApp.cs
+++ src/CustomApp/MyApp.cs
@@ -12,10 +12,16 @@ namespace app
         public override async Task InitializeAsync()
         {
             Log("Listing for events with Rx");
-            IObservable<EntityState> obs = this;
-            this.ForEach(e =>
+
+            this.Subscribe(e =>
             {
                 Log("Rx - got global event for {0}, state is {1}", e.EntityId, e.State);
+            });
+
+            Log("listening for specific entity events with Rx");
+            Entity("light.stairs").Subscribe(e =>
+            {
+                Log("Rx - got specific light.stairs event for {0}: {1}", e.EntityId, e.State);
             });
 
         }
