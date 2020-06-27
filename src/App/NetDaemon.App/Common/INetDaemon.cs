0ff5ae75b6c76a0b8de9e04d310ed8d203885b2f
BeeHiveJava <BeeHiveJava@users.noreply.github.com>
2020-06-27 17:28:27 +0200

    Add log information calls (#144)
    * feat: add calls to log informational messages
    
    This change adds the various helper methods to log informational messages. This is more in line with the original logging interface provided by Microsoft. Also this helps people log informational messages without specifying additional arguments that they don't always have available.
    
    Closes 143
    
    * docs: fix doc for debug logging
    
    * style: remove inconsistent whitespacing
    
    Co-authored-by: Tomas Hellström <tomas.hellstrom@yahoo.se>

--- src/App/NetDaemon.App/Common/INetDaemon.cs
+++ src/App/NetDaemon.App/Common/INetDaemon.cs
@@ -414,29 +414,56 @@ namespace JoySoftware.HomeAssistant.NetDaemon.Common
     public interface IDoLogging
     {
         /// <summary>
-        ///     Logs a informational message
+        ///     Logs an informational message
         /// </summary>
         /// <param name="exception">A exception</param>
         /// <param name="message">The message to log</param>
         /// <param name="param">Params</param>
         void Log(Exception exception, string message, params object[] param);
-
+        
+        /// <summary>
+        ///     Logs an informational message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        void LogInformation(string message);
 
         /// <summary>
-        ///     Logs a informational message
+        ///     Logs an informational message
+        /// </summary>
+        /// <param name="exception">A exception</param>
+        /// <param name="message">The message to log</param>
+        void LogInformation(Exception exception, string message);
+
+        /// <summary>
+        ///     Logs an informational message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="param">Params</param>
+        void LogInformation(string message, params object[] param);
+
+        /// <summary>
+        ///     Logs an informational message
+        /// </summary>
+        /// <param name="exception">A exception</param>
+        /// <param name="message">The message to log</param>
+        /// <param name="param">Params</param>
+        void LogInformation(Exception exception, string message, params object[] param);
+        
+        /// <summary>
+        ///     Logs a debug message
         /// </summary>
         /// <param name="message">The message to log</param>
         void LogDebug(string message);
 
         /// <summary>
-        ///     Logs a informational message
+        ///     Logs a debug message
         /// </summary>
         /// <param name="exception">A exception</param>
         /// <param name="message">The message to log</param>
         void LogDebug(Exception exception, string message);
 
         /// <summary>
-        ///     Logs a informational message
+        ///     Logs a debug message
         /// </summary>
         /// <param name="message">The message to log</param>
         /// <param name="param">Params</param>
@@ -462,6 +489,7 @@ namespace JoySoftware.HomeAssistant.NetDaemon.Common
         /// <param name="exception">A exception</param>
         /// <param name="message">The message to log</param>
         void LogError(Exception exception, string message);
+        
         /// <summary>
         ///     Logs an error message
         /// </summary>
@@ -476,7 +504,6 @@ namespace JoySoftware.HomeAssistant.NetDaemon.Common
         /// <param name="message">The message to log</param>
         /// <param name="param">Params</param>
         void LogError(Exception exception, string message, params object[] param);
-
 
         /// <summary>
         ///     Logs a trace message
