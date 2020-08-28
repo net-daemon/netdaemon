1d9a224e0b6a70679218fc1385c744383c9e9820
helto4real <tomas.hellstrom@yahoo.se>
2020-08-28 13:03:35 +0200

    Default not specify timeout value

--- src/App/NetDaemon.App/Common/Reactive/ObservableExtensionMethods.cs
+++ src/App/NetDaemon.App/Common/Reactive/ObservableExtensionMethods.cs
@@ -20,14 +20,23 @@ namespace NetDaemon.Common.Reactive
         }
 
         /// <summary>
-        ///     Is same for timespan time
+        ///     Wait for state the specified
         /// </summary>
         /// <param name="observable"></param>
         /// <param name="timeout">Timeout waiting for state</param>
-        /// <returns></returns>
-        public static IObservable<(EntityState Old, EntityState New)> NDFirstOrTimeout(this IObservable<(EntityState Old, EntityState New)> observable, TimeSpan timeout)
+        public static IObservable<(EntityState Old, EntityState New)> NDWaitForState(this IObservable<(EntityState Old, EntityState New)> observable, TimeSpan timeout)
         {
             return observable.Timeout(timeout, Observable.Return((new NetDaemon.Common.EntityState() { State = "TimeOut" }, new NetDaemon.Common.EntityState() { State = "TimeOut" }))).Take(1);
         }
+
+        /// <summary>
+        ///     Wait for state the default time
+        /// </summary>
+        /// <param name="observable"></param>
+        public static IObservable<(EntityState Old, EntityState New)> NDWaitForState(this IObservable<(EntityState Old, EntityState New)> observable)
+        {
+            return observable.Timeout(TimeSpan.FromSeconds(5), Observable.Return((new NetDaemon.Common.EntityState() { State = "TimeOut" }, new NetDaemon.Common.EntityState() { State = "TimeOut" }))).Take(1);
+        }
+
     }
 }
