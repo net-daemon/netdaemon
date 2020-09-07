b879cbb16656b095fdd6f9a2838c0ca496dbe043
Tomas Hellström <tomas.hellstrom@yahoo.se>
2020-09-07 09:52:12 +0200

    Added possibility to test (#200)
    * Added possibility to test
    
    * Fix
    
    * Default not specify timeout value
    
    * typo

--- src/App/NetDaemon.App/Common/Reactive/ObservableExtensionMethods.cs
+++ src/App/NetDaemon.App/Common/Reactive/ObservableExtensionMethods.cs
@@ -18,5 +18,25 @@ namespace NetDaemon.Common.Reactive
         {
             return observable.Throttle(span);
         }
+
+        /// <summary>
+        ///     Wait for state the specified time
+        /// </summary>
+        /// <param name="observable"></param>
+        /// <param name="timeout">Timeout waiting for state</param>
+        public static IObservable<(EntityState Old, EntityState New)> NDWaitForState(this IObservable<(EntityState Old, EntityState New)> observable, TimeSpan timeout)
+        {
+            return observable.Timeout(timeout, Observable.Return((new NetDaemon.Common.EntityState() { State = "TimeOut" }, new NetDaemon.Common.EntityState() { State = "TimeOut" }))).Take(1);
+        }
+
+        /// <summary>
+        ///     Wait for state the default time
+        /// </summary>
+        /// <param name="observable"></param>
+        public static IObservable<(EntityState Old, EntityState New)> NDWaitForState(this IObservable<(EntityState Old, EntityState New)> observable)
+        {
+            return observable.Timeout(TimeSpan.FromSeconds(5), Observable.Return((new NetDaemon.Common.EntityState() { State = "TimeOut" }, new NetDaemon.Common.EntityState() { State = "TimeOut" }))).Take(1);
+        }
+
     }
 }
