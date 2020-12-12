804d94c801c2096ef6f9a996326d9aacb9575ac7
helto4real <tomas.hellstrom@yahoo.se>
2020-12-12 20:41:51 +0100

    Changed to linux-arm

--- Docker/build_dotnet.sh
+++ Docker/build_dotnet.sh
@@ -6,7 +6,7 @@ echo "UNAME IS!!!!: $ARCH"
 if [ $ARCH == "armv7l" ]; then
     dotnet publish /usr/src/Service/Service.csproj -v q -c Release -r "linux-arm" -o "/daemon"
 elif [ $ARCH == "aarch64" ]; then
-    dotnet publish /usr/src/Service/Service.csproj -v q -c Release -r "linux-arm64" -o "/daemon"
+    dotnet publish /usr/src/Service/Service.csproj -v q -c Release -r "linux-arm" -o "/daemon"
 elif [ $ARCH == "x86_64" ]; then
     dotnet publish /usr/src/Service/Service.csproj -v q -c Release -r "linux-x64" -o "/daemon"
 else
