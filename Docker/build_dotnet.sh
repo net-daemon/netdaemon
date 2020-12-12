58b87ccf5acbff44c5a721db6d964fd5e335591d
helto4real <tomas.hellstrom@yahoo.se>
2020-12-12 18:57:05 +0100

    Fix arm 64

--- Docker/build_dotnet.sh
+++ Docker/build_dotnet.sh
@@ -4,7 +4,7 @@ ARCH=$(uname -m)
 if [ $ARCH == "armv7l" ]; then
     dotnet publish /usr/src/Service/Service.csproj -v q -c Release -r "linux-arm" -o "/daemon"
 elif [ $ARCH == "aarch64" ]; then
-    dotnet publish /usr/src/Service/Service.csproj -v q -c Release -r "linux-arm" -o "/daemon"
+    dotnet publish /usr/src/Service/Service.csproj -v q -c Release -r "linux-arm64" -o "/daemon"
 elif [ $ARCH == "x86_64" ]; then
     dotnet publish /usr/src/Service/Service.csproj -v q -c Release -r "linux-x64" -o "/daemon"
 else
