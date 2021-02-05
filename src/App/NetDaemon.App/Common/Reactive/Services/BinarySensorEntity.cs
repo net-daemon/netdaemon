5da7839417b3115805ca2a26ccfb1ee96c7aba3f
Filip STAS <filip@visuasoft.be>
2021-02-05 17:10:17 +0100

    Started adding xml doc

--- src/App/NetDaemon.App/Common/Reactive/Services/BinarySensorEntity.cs
+++ src/App/NetDaemon.App/Common/Reactive/Services/BinarySensorEntity.cs
@@ -2,9 +2,10 @@
 
 namespace NetDaemon.Common.Reactive.Services
 {
+    /// <inheritdoc />
     public  class BinarySensorEntity : RxEntityBase
     {
-
+        /// <inheritdoc />
         public BinarySensorEntity(INetDaemonRxApp daemon, IEnumerable<string> entityIds): base(daemon, entityIds)
         {
         }
