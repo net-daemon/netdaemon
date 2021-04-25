18a883fdfce0796855e57fa1cfd6c88c2139b2a9
Frank Bakker <Bakker.Frank@gmail.com>
2021-04-25 22:48:47 +0200

    Cleaned up

--- src/DaemonRunner/DaemonRunner/Service/App/CodeGenerator.cs
+++ src/DaemonRunner/DaemonRunner/Service/App/CodeGenerator.cs
@@ -2,17 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using JoySoftware.HomeAssistant.Client;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.Extensions.Logging.EventSource;
-using NetDaemon.Common;
-using NetDaemon.Common.Exceptions;
 using NetDaemon.Common.Reactive;
 using NetDaemon.Common.Reactive.Services;
 using NetDaemon.Daemon.Config;
-using Newtonsoft.Json;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 [assembly: InternalsVisibleTo("NetDaemon.Daemon.Tests")]
@@ -21,7 +18,7 @@ namespace NetDaemon.Service.App
 {
     public static class CodeGenerator
     {
-        private static readonly Dictionary<string, string[]> _skipDomainServices = new Dictionary<string, string[]>()
+        private static readonly Dictionary<string, string[]> _skipDomainServices = new()
         {
             {"lock", new[] {"lock", "unlock", "open"}},
             {"light", new[] {"turn_on", "turn_off", "toggle"}},
@@ -93,11 +90,14 @@ namespace NetDaemon.Service.App
             }
         };
 
-        public static string? GenerateCodeRx(string nameSpace, IReadOnlyCollection<string> entities,
+        public static string GenerateCodeRx(string nameSpace, IReadOnlyCollection<string> entities,
             IReadOnlyCollection<HassServiceDomain> serviceDomains)
         {
             var entityDomains = GetDomainsFromEntities(entities).OrderBy(s => s).ToList();
-            var allDomains = entityDomains.Union(serviceDomains.Select(sd => sd.Domain)).OfType<string>()
+            var serviceDomainsWithOperations =
+                serviceDomains.Where(sd => sd.Services?.Any(s => !HasEntityIdArgument(s)) == true);
+
+            var allDomains = entityDomains.Union(serviceDomainsWithOperations.Select(sd => sd.Domain)).OfType<string>()
                 .OrderBy(s => s).ToList();
 
             var code = CompilationUnit()
@@ -105,30 +105,27 @@ namespace NetDaemon.Service.App
                        .AddUsings(UsingDirective(ParseName(typeof(RxEntityBase).Namespace!)))
                        .AddUsings(UsingDirective(ParseName(typeof(NetDaemonRxApp).Namespace!)));
 
-            // Add namespace
-            var namespaceDeclaration = NamespaceDeclaration(ParseName(nameSpace)).NormalizeWhitespace();
+           var namespaceDeclaration = NamespaceDeclaration(ParseName(nameSpace)).NormalizeWhitespace();
 
-            var appBaseClassDeclaration = GenerateAppBaseClass(allDomains);
+           // One Base class
+           var appBaseClassDeclaration = GenerateAppBaseClass(allDomains);
 
             namespaceDeclaration = namespaceDeclaration.AddMembers(appBaseClassDeclaration);
 
+            var serviceDomainByName = serviceDomains.ToLookup(sd => sd.Domain);
+
+            // Typed entity classes
             foreach (var entityDomain in entityDomains)
             {
-
-                var serviceDomain = serviceDomains.FirstOrDefault(sd => sd.Domain == entityDomain);
-
-                // all entity domains, with the service domain if it exists
-                var entityClass = GenerateEntityClass(entityDomain, serviceDomain);
+                var entityClass = GenerateEntityClass(entityDomain, serviceDomainByName[entityDomain].FirstOrDefault());
 
                 namespaceDeclaration = namespaceDeclaration.AddMembers(entityClass);
             }
 
-
-            // Add the classes implementing the specific entities
+            // Domain classes
             foreach (var entityDomain in allDomains)
             {
-                var serviceDomain = serviceDomains.FirstOrDefault(sd => sd.Domain == entityDomain);
-                var entityClass = GenerateDomainClass(entityDomain, serviceDomain, entities);
+                var entityClass = GenerateDomainClass(entityDomain, serviceDomainByName[entityDomain].FirstOrDefault(), entities);
 
                 namespaceDeclaration = namespaceDeclaration.AddMembers(entityClass);
             }
@@ -141,138 +138,116 @@ namespace NetDaemon.Service.App
         private static ClassDeclarationSyntax GenerateDomainClass(string domainName, HassServiceDomain? serviceDomain, IEnumerable<string> entities)
         {
 
-            var classDeclaration = $@"public partial class {domainName?.ToCamelCase()}Entities
-    {{
-        private readonly {nameof(NetDaemonRxApp)} _app;
+            var classDeclaration = $@"public partial class {domainName.ToCamelCase()}Entities
+                                      {{
+                                          private readonly {nameof(NetDaemonRxApp)} _app;
 
-        public {domainName.ToCamelCase()}Entities( {nameof(NetDaemonRxApp)} app)
-        {{
-            _app = app;
-        }}
-    }}";
-            var entityClass = CSharpSyntaxTree.ParseText(classDeclaration).GetRoot().ChildNodes()
-                                  .OfType<ClassDeclarationSyntax>().First();
+                                          public {domainName.ToCamelCase()}Entities( {nameof(NetDaemonRxApp)} app)
+                                          {{
+                                              _app = app;
+                                          }}
+                                      }}";
 
-            foreach (var entity in entities.Where(n =>
-                n.StartsWith(domainName, StringComparison.InvariantCultureIgnoreCase)))
-            {
-                var propDeclaration = GenerateEntityProperty(entity, domainName);
-                entityClass = entityClass.AddMembers(propDeclaration);
-            }
+            var entityClassDeclaration = Parse<ClassDeclarationSyntax>(classDeclaration);
+
+            var domainEntities = entities.Where(n => n.StartsWith(domainName + ".", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+            var propertyDeclaration = domainEntities.Select(e => GenerateEntityProperty(e, domainName)).ToArray();
+
+            entityClassDeclaration = entityClassDeclaration.AddMembers(propertyDeclaration);
+
+            var usedNames = propertyDeclaration.Select(p => p.Identifier.Text).ToHashSet();
 
             // Generate methods for all services that do not require an entity ID
-            // TODO: detect name collisions with entities
-            if (serviceDomain?.Services is not null)
-            {
-                var nonEntityMethods = serviceDomain.Services?.Where(s => !HasEntityIdArgument(s)).SelectMany(s =>
-                    GenerateServiceMethods(domainName, s, "_app."));
+            if (serviceDomain?.Services is null) return entityClassDeclaration;
 
-                entityClass = entityClass.AddMembers(nonEntityMethods.ToArray());
-            }
+            var nonEntityMethods = serviceDomain.Services.Where(s => !HasEntityIdArgument(s)).SelectMany(s =>
+                GenerateServiceMethods(domainName, s, usedNames, "_app."));
 
-            return entityClass;
+            entityClassDeclaration = entityClassDeclaration.AddMembers(nonEntityMethods.ToArray());
+
+            return entityClassDeclaration;
         }
 
+        private static T Parse<T>(string text) =>  CSharpSyntaxTree.ParseText(text).GetRoot().ChildNodes().OfType<T>().First();
+
+        /// <summary>
+        /// Generate property to get type entity
+        /// </summary>
+        /// <returns></returns>
         private static PropertyDeclarationSyntax GenerateEntityProperty(string entity, string domain)
         {
-            // generate property like
-
             // public SwitchEntity LivingRoomSwitch => new(_app, new string[] { "switch.living_room_switch" });
 
             var name = entity[(entity.IndexOf(".", StringComparison.InvariantCultureIgnoreCase) + 1)..];
-            // Quick check to make sure the name is a valid C# identifier. Should really check to make
-            // sure it doesn't collide with a reserved keyword as well.
-            if (!char.IsLetter(name[0]) && (name[0] != '_'))
-            {
-                name = "e_" + name;
-            }
+            name = MakeValidName(name, null, "e_", null);
 
-            var propertyCode =
-                $@"public {domain.ToCamelCase()}Entity {name.ToCamelCase()} => new(_app, new string[] {{""{entity}""}});";
-            var propDeclaration = CSharpSyntaxTree.ParseText(propertyCode).GetRoot().ChildNodes()
-                                      .OfType<PropertyDeclarationSyntax>().FirstOrDefault()
-                                  ?? throw new NetDaemonNullReferenceException("Failed to parse property");
+            var propertyCode =  $@"public {domain.ToCamelCase()}Entity {name.ToCamelCase()} => new(_app, new string[] {{""{entity}""}});";
+
+            var propDeclaration = Parse<PropertyDeclarationSyntax>(propertyCode);
+
             return propDeclaration;
         }
 
-        private static bool HasEntityIdArgument(HassService service) => service.Fields.Any(f => IsEntityId(f));
-
-        private static bool IsEntityId(HassServiceField f) => f.Field == "entity_id";
-
-        private static ClassDeclarationSyntax GenerateEntityClass(string domainName, HassServiceDomain serviceDomain)
+        private static ClassDeclarationSyntax GenerateEntityClass(string domainName, HassServiceDomain? serviceDomain)
         {
             var baseClass = _skipDomainServices.ContainsKey(domainName)
                 ? $"{typeof(RxEntityBase).Namespace}.{domainName.ToCamelCase()}Entity"
                 : $"{typeof(RxEntityBase).Namespace}.RxEntityBase";
 
-            var classDeclaration = $@"public partial class {domainName.ToCamelCase()}Entity : {baseClass}
-{{
-        public {domainName.ToCamelCase()}Entity(INetDaemonRxApp daemon, IEnumerable<string> entityIds) : base(daemon, entityIds)
-        {{
-        }}
-    }}";
-            var entityClass = CSharpSyntaxTree.ParseText(classDeclaration).GetRoot().ChildNodes()
-                                  .OfType<ClassDeclarationSyntax>().FirstOrDefault()
-                              ?? throw new NetDaemonNullReferenceException("Failed to parse class declaration");
+            var classDeclaration = $@"  public partial class {domainName.ToCamelCase()}Entity : {baseClass}
+                                        {{
+                                                public {domainName.ToCamelCase()}Entity(INetDaemonRxApp daemon, IEnumerable<string> entityIds) : base(daemon, entityIds)
+                                                {{
+                                                }}
+                                            }}";
+
+            var entityClass = Parse<ClassDeclarationSyntax>(classDeclaration);
 
             // Generate methods for all services that do require an entity ID
-            // TODO: detect name collisions with entities
-            if (serviceDomain?.Services is not null)
-            {
-                var nonEntityMethods = serviceDomain.Services?.Where(s => HasEntityIdArgument(s)).SelectMany(s =>
-                    GenerateServiceMethods(domainName, s));
+            if (serviceDomain?.Services is null) return entityClass;
 
-                entityClass = entityClass.AddMembers(nonEntityMethods.ToArray());
-            }
+            var nonEntityMethods = serviceDomain.Services.Where(HasEntityIdArgument).SelectMany(s =>
+                GenerateServiceMethods(domainName, s, new()));
+
+            entityClass = entityClass.AddMembers(nonEntityMethods.ToArray());
 
             return entityClass;
         }
 
-        private static IEnumerable<MemberDeclarationSyntax> GenerateServiceMethods(string domain, HassService service, string prefix = "")
+        private static IEnumerable<MemberDeclarationSyntax> GenerateServiceMethods(string domain, HassService service, HashSet<string> usedNames, string callPrefix = "")
         {
-            var name = service.Service[(service.Service.IndexOf(".", StringComparison.InvariantCultureIgnoreCase) + 1)..];
+            var serviceMethodName = service.Service?[(service.Service.IndexOf(".", StringComparison.InvariantCultureIgnoreCase) + 1)..];
 
             List<MemberDeclarationSyntax> result = new();
 
-            name = name.ToCamelCase();
+            serviceMethodName = MakeValidName(serviceMethodName!, usedNames, "s_", "Service");
 
-            // Quick check to make sure the name is a valid C# identifier. Should really check to make
-            // sure it doesn't collide with a reserved keyword as well.
-
-            if (!char.IsLetter(name[0]) && (name[0] != '_'))
-            {
-                name = "s_" + name;
-            }
-
-            var argumentsRecord = GenerateServiceArgsRecord(name, service);
+            var argumentsRecord = GenerateServiceArgsRecord(serviceMethodName, service);
             var argsTypeName = argumentsRecord.Identifier.Text;
-
 
             var hasEntityId = HasEntityIdArgument(service);
             var hasEntityIdString = hasEntityId ? "true" : "false";
-            var methodCode = $@"public void {name}({argsTypeName}? data=null)
-                    {{
-                        {prefix}CallService(""{domain}"", ""{service.Service}"", data,{hasEntityIdString});
-                    }}
-                    ";
-            var methodDeclaration = CSharpSyntaxTree.ParseText(methodCode).GetRoot().ChildNodes()
-                                    .OfType<GlobalStatementSyntax>().FirstOrDefault()
-                                ?? throw new NetDaemonNullReferenceException("Failed to parse method");
+            var methodCode = $@"public void {serviceMethodName}({argsTypeName}? data=null)
+                                {{
+                                    {callPrefix}CallService(""{domain}"", ""{service.Service}"", data,{hasEntityIdString});
+                                }}";
+
+            var methodDeclaration = Parse<GlobalStatementSyntax>(methodCode);
+
             result.Add(methodDeclaration);
 
-            if (service.Fields.Count() == (hasEntityId ? 2 : 1))
+            if (service.Fields?.Count() == (hasEntityId ? 2 : 1))
             {
                 var paramName = service.Fields.Single(f => f.Field != "entity_id").Field;
 
                 // We have a single prop besides the id, generate a simple wrapper method with one argument
-                var methodCode2 = $@"public void {name}(string {paramName})
-                    {{
-                        {prefix}CallService(""{domain}"", ""{service.Service}"", new {{ {paramName} = {paramName} }} ,{hasEntityIdString});
-                    }}
-                    ";
-                var methodDeclaration2 = CSharpSyntaxTree.ParseText(methodCode2).GetRoot().ChildNodes()
-                                             .OfType<GlobalStatementSyntax>().FirstOrDefault()
-                                         ?? throw new NetDaemonNullReferenceException("Failed to parse method");
+                var methodCode2 = $@"public void {serviceMethodName}(string {paramName})
+                                     {{
+                                         {callPrefix}CallService(""{domain}"", ""{service.Service}"", new {{ {paramName} = {paramName} }} ,{hasEntityIdString});
+                                     }}";
+
+                var methodDeclaration2 = Parse<GlobalStatementSyntax>(methodCode2);
 
                 result.Add(methodDeclaration2);
             }
@@ -292,7 +267,7 @@ namespace NetDaemon.Service.App
 
             // Get all available domains, this is used to create the extension methods
 
-            var singleServiceDomains = new string[] {"script"};
+            var singleServiceDomains = new [] {"script"};
             foreach (var domain in domains)
             {
                 var camelCaseDomain = domain.ToCamelCase();
@@ -303,10 +278,7 @@ namespace NetDaemon.Service.App
                     ? $"public {camelCaseDomain}Entities {camelCaseDomain} => new(this);"
                     : $@"public {camelCaseDomain}Entity {camelCaseDomain} => new(this, new string[] {{""""}});";
 
-                var propertyDeclaration = CSharpSyntaxTree.ParseText(property).GetRoot().ChildNodes()
-                                              .OfType<PropertyDeclarationSyntax>().FirstOrDefault()
-                                          ?? throw new NetDaemonNullReferenceException(
-                                              $"Parse of property {camelCaseDomain} Entities/Entity failed");
+                var propertyDeclaration = Parse<PropertyDeclarationSyntax>(property);
                 extensionClass = extensionClass.AddMembers(propertyDeclaration);
             }
 
@@ -319,14 +291,14 @@ namespace NetDaemon.Service.App
                 .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword)))
                 .WithOpenBraceToken(Token(SyntaxKind.OpenBraceToken));
 
-            foreach (var field in s.Fields)
+            foreach (var field in s.Fields ?? Enumerable.Empty<HassServiceField>())
             {
                 // TODO: make the prop camel case but serialize with original name
                 var propCode = //$"[JsonProperty(propertyName:{name})]" +
-                               $"public string {field.Field} {{ get; init; }}";
-                var prop = CSharpSyntaxTree.ParseText(propCode);
-                var p = prop.GetRoot().ChildNodes().OfType<MemberDeclarationSyntax>().FirstOrDefault();
-                record = record.AddMembers(p);
+                               $"public object {field.Field} {{ get; init; }}";
+
+                var prop = Parse<MemberDeclarationSyntax>(propCode);
+                record = record.AddMembers(prop);
             }
 
             record = record.WithCloseBraceToken(
@@ -334,20 +306,33 @@ namespace NetDaemon.Service.App
             return record.NormalizeWhitespace();
         }
 
+        private static bool HasEntityIdArgument(HassService service) => service.Fields?.Any(IsEntityId) == true;
+
+        private static bool IsEntityId(HassServiceField f) => f.Field == "entity_id";
+
         /// <summary>
         ///     Returns a list of domains from all entities
         /// </summary>
         /// <param name="entities">A list of entities</param>
         internal static IEnumerable<string> GetDomainsFromEntities(IEnumerable<string> entities) =>
-            entities.Select(n => n[0..n.IndexOf(".", StringComparison.InvariantCultureIgnoreCase)]).Distinct();
+            entities.Select(n => n[..n.IndexOf(".", StringComparison.InvariantCultureIgnoreCase)]).Distinct();
 
-        private static bool ShouldGenerateDomainEntity(string domain, IEnumerable<HassServiceDomain> services)
+        private static string MakeValidName(string name, HashSet<string>? usedNames, string prefix, string? suffix)
         {
-            if (!_skipDomainServices.ContainsKey(domain)) return true;
-            var domainServiceNames = services.Where(n => n.Domain == domain)
-                .SelectMany(n => n.Services ?? new List<HassService>()).Select(s =>
-                    s.Service![(s.Service.IndexOf(".", StringComparison.InvariantCultureIgnoreCase) + 1)..]);
-            return domainServiceNames.Except(_skipDomainServices[domain]).Any();
+            name = name.ToCamelCase();
+            name = Regex.Replace(name, @"[^\w]", "");
+
+            if (!char.IsLetter(name[0]) && (name[0] != '_'))
+            {
+                name = prefix + name;
+            }
+
+            if (usedNames !=null && !usedNames.Add(name))
+            {
+                name += suffix;
+            }
+
+            return name;
         }
     }
 }
