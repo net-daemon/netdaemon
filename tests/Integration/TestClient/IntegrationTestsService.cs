f452950833fd068d4b8ad39e7dfc47b437fd7418
helto4real <tomas.hellstrom@yahoo.se>
2021-05-26 12:22:27 +0200

    added testcase

--- tests/Integration/TestClient/IntegrationTestsService.cs
+++ tests/Integration/TestClient/IntegrationTestsService.cs
@@ -23,22 +23,48 @@ namespace TestClient
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            Environment.ExitCode = -1;
+
             var daemonHost  = _serviceProvider.GetService<NetDaemonHost>();
             if (daemonHost is null)
             {
                 Debug.Fail("Faild to get daemonHost!");
-                Environment.ExitCode = -1;
                 _globalCancellationSource.Cancel();
                return;
             }
             while (!stoppingToken.IsCancellationRequested && !daemonHost.IsConnected) {
                 await Task.Delay(2000, stoppingToken).ConfigureAwait(false);
             }
-            System.Console.WriteLine("CONNECTED TO DAEMON");
+            // Test case #1 call service and check correct state 
+
             daemonHost.CallService("input_select", "select_option", new {entity_id = "input_select.who_cooks", option="Paulus"}); //.ConfigureAwait(false);
             await Task.Delay(2000, stoppingToken).ConfigureAwait(false);
+
+            if (FailEq<string>(daemonHost.GetState("input_select.who_cooks")?.State, "Paulus"))
+                return;
+            // End test case #1
+
             Environment.ExitCode = 0;
             _globalCancellationSource.Cancel();
+        }
+
+        private static bool FailEq<T>(T actual, T expected)
+        {
+            var IsEqual = actual?.Equals(expected) ?? false;
+            if (!IsEqual)
+            {
+                Console.WriteLine($"EXPECTED: {expected}, GOT: {actual}");
+            }
+            return Fail(IsEqual);
+        }
+        private static bool Fail(bool check)
+        {
+            if (!check)
+            {
+                Environment.ExitCode = -1;
+                _globalCancellationSource.Cancel();
+            }
+            return !check;
         }
     }
 }
