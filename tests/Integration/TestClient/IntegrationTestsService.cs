85d7c40cb5942ae5157b7279fc88d4795b952c30
helto4real <tomas.hellstrom@yahoo.se>
2021-05-26 12:44:36 +0200

    fake error

--- tests/Integration/TestClient/IntegrationTestsService.cs
+++ tests/Integration/TestClient/IntegrationTestsService.cs
@@ -52,7 +52,7 @@ namespace TestClient
             daemonHost.CallService("input_select", "select_option", new {entity_id = "input_select.who_cooks", option="Anne Therese"}, true); //.ConfigureAwait(false);
             await Task.Delay(300, stoppingToken).ConfigureAwait(false);
 
-            if (FailEq<string>(daemonHost.GetState("input_select.who_cooks")?.State, "Anne Therese"))
+            if (FailEq<string>(daemonHost.GetState("input_select.who_cooks")?.State, "Paulus"))
                 return;
 
             Environment.ExitCode = 0;
