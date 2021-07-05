a41b2222ac2eea64ff980580a518422d853adcd5
helto4real <tomas.hellstrom@yahoo.se>
2021-07-05 21:58:59 +0200

    cleanup

--- src/Daemon/NetDaemon.Daemon/Daemon/EntityStateManagement.cs
+++ src/Daemon/NetDaemon.Daemon/Daemon/EntityStateManagement.cs
@@ -43,7 +43,7 @@ namespace NetDaemon.Daemon
         public void Store(HassState newState) => InternalState[newState.EntityId] = newState;
 
         private readonly string[] _supportedDomains = {"binary_sensor", "sensor", "switch"};
-        
+
         public async Task<HassState?> SetStateAndWaitForResponseAsync(string entityId, string? state,
             object? attributes, bool waitForResponse)
         {
