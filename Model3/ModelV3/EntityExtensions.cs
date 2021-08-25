5bec3b5a5759128e2a6a18e5b8cb438598615e49
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-08-25 13:02:05 +0200

    Added IsOn and IsOff to EntityState

--- Model3/ModelV3/EntityExtensions.cs
+++ Model3/ModelV3/EntityExtensions.cs
@@ -4,8 +4,13 @@ namespace NetDaemon.Common.ModelV3
 {
     public static class EntityExtensions
     {
-        public static bool IsOn(this Entity entity) => string.Equals(entity.State, "on", StringComparison.OrdinalIgnoreCase);
+        public static bool IsOn(this EntityState entity) => string.Equals(entity.State, "on", StringComparison.OrdinalIgnoreCase);
         
-        public static bool IsOff(this Entity entity) => string.Equals(entity.State, "off", StringComparison.OrdinalIgnoreCase);
+        public static bool IsOff(this EntityState entity) => string.Equals(entity.State, "off", StringComparison.OrdinalIgnoreCase);
+
+        public static bool IsOn(this Entity entity) => entity.EntityState?.IsOn() ?? false;
+        
+        public static bool IsOff(this Entity entity) => entity.EntityState?.IsOff() ?? false;
+
     }
 }
