7aa973e0fdc13349fc00d8fe48f7eb59590bec79
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-08-26 08:14:11 +0200

    Strong typed events (#437)

---
+++ Model3/ModelV3/IEventProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NetDaemon.Common;
+
+namespace Model3.ModelV3
+{
+    public interface IEventProvider
+    {
+        // TODO: not sure yet if this should be part of IHaCOntext or as a separate interface
+        
+        IObservable<T> GetEventDataOfType<T>(string eventType) where T : class;
+    }
+}
