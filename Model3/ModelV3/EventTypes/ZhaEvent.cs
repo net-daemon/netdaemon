e2434b9ecfbc978c1f5d25b5ecd617a9f6b04674
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-08-26 08:14:11 +0200

    Strong typed events (#437)

---
+++ Model3/ModelV3/EventTypes/ZhaEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Model3.ModelV3.EventTypes
+{
+    public record ZhaEventData
+    {
+        public string device_ieee { get; set; }
+        public string unique_id { get; set; }
+        public int endpoint_id { get; set; }
+        public int cluster_id { get; set; }
+        public string command { get; set; }
+        public object[] args { get; set; }
+    }
+}
