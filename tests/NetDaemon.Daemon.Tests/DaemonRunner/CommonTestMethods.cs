54b4402819565bb5d2fdb2d4cf54185b79950d64
Zorgino <maxsu4@gmail.com>
2021-09-03 17:38:02 +0300

    Yaml config should not instantiate apps (#450)
    * move app instantiation outside of yaml config
    
    * add missing test
    
    * fix warnings. rename test
    
    * fix logging

---
+++ tests/NetDaemon.Daemon.Tests/DaemonRunner/CommonTestMethods.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NetDaemon.Daemon.Config;
+
+namespace NetDaemon.Daemon.Tests.DaemonRunner
+{
+    public static class CommonTestMethods
+    {
+        public static Mock<IIoWrapper> GetIOWrapperMock(string text)
+        {
+            var ioWrapperMock = new Mock<IIoWrapper>();
+            ioWrapperMock
+                .Setup(x => x.ReadFile(It.IsAny<string>()))
+                .Returns(() => text);
+
+            return ioWrapperMock;
+        }
+
+        public static YamlConfigReader GetYamlConfigReader(string yamlTextToReturn)
+        {
+            return new YamlConfigReader(GetIOWrapperMock(yamlTextToReturn).Object);
+        }
+
+        public static YamlConfigReader GetYamlConfigReader()
+        {
+            return new YamlConfigReader(new IoWrapper());
+        }
+    }
+}
