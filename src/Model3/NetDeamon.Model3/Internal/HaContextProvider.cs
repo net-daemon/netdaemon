76a4a6380f354c394522afad4b2e99b0899c7c0c
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-09-04 11:17:11 +0200

    Isolated observable (#451)
    * Draft IsolatedObservables
    
    * Added Test
    
    * cleand ebug app

--- src/Model3/NetDeamon.Model3/Internal/HaContextProvider.cs
+++ src/Model3/NetDeamon.Model3/Internal/HaContextProvider.cs
@@ -1,30 +1,33 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Reactive.Linq;
 using JoySoftware.HomeAssistant.Client;
 using JoySoftware.HomeAssistant.Model;
+using System.Diagnostics.CodeAnalysis;
+using NetDaemon.Infrastructure.ObservableHelpers;
 using NetDaemon.Model3.Common;
 using NetDaemon.Model3.Entities;
 
 namespace NetDaemon.Model3.Internal
 {
     [SuppressMessage("", "CA1812", Justification = "Is Loaded via DependencyInjection")]
-    internal class HaContextProvider : IHaContext
+    internal class HaContextProvider : IHaContext, IDisposable, IEventProvider
     {
-        private readonly IObservable<HassEvent> _hassEventObservable;
         private readonly EntityStateCache _entityStateCache;
         private readonly IHassClient _hassClient;
+        private readonly ScopedObservable<HassEvent> _scopedObservable;
 
-        public HaContextProvider(
-            IObservable<HassEvent> hassEventObservable,
+        public HaContextProvider(IObservable<HassEvent> hassEventObservable,
             EntityStateCache entityStateCache,
             IHassClient hassClient)
         {
-            _hassEventObservable = hassEventObservable;
             _entityStateCache = entityStateCache;
             _hassClient = hassClient;
 
-            var stateChanges = _hassEventObservable
+            // Create a new ScopedObservable from the one we got
+            // this makes sure we will unsubscribe then this ContextProvider is Disposed
+            _scopedObservable = new ScopedObservable<HassEvent>(hassEventObservable);
+
+            var stateChanges = _scopedObservable
                 .Select(e => e.Data as HassStateChangedEventData)
                 .Where(e => e != null)
                 .Select(e => e!);
@@ -47,14 +50,21 @@ namespace NetDaemon.Model3.Internal
 
         public void CallService(string domain, string service, object? data, Entity entity)
         {
-            _hassClient.CallService(domain, service,
-                data,
-                new HassTarget { EntityIds = new[] { entity.EntityId } });
+            _hassClient.CallService(domain, service, data, new HassTarget { EntityIds = new[] { entity.EntityId } });
         }
 
-        //public IRxEvent EventChanges { get; }
         public IObservable<StateChange> StateAllChanges { get; }
 
         public IObservable<StateChange> StateChanges => StateAllChanges.Where(e => e.New?.State != e.Old?.State);
+
+        public IObservable<T> GetEventDataOfType<T>(string eventType) where T : class => 
+            _scopedObservable
+                .Where(e => e.EventType == eventType && e.DataElement != null)
+                .Select(e => e.DataElement?.ToObject<T>()!);
+
+        public void Dispose()
+        {
+            _scopedObservable.Dispose();
+        }
     }
 }
