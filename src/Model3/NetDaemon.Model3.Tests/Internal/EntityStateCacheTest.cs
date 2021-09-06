f646da258bfdc5f37fa23fa52e4bda22ead42b95
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-09-06 13:24:03 +0200

    Make EntityStateCache store states once and forward events to apps (#456)
    * Make EntityStateCache store states once and forward events to apps
    
    * review fix

---
+++ src/Model3/NetDaemon.Model3.Tests/Internal/EntityStateCacheTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reactive.Subjects;
+using JoySoftware.HomeAssistant.Client;
+using JoySoftware.HomeAssistant.Model;
+using Moq;
+using NetDaemon.Model3.Internal;
+using Xunit;
+
+namespace NetDaemon.Model3.Tests.Internal
+{
+    public class EntityStateCacheTest
+    {
+        [Fact]
+        public void StateChangeEventIsFirstStoredInCacheThanForwarded()
+        {
+            // Arrange
+            using var testSubject = new Subject<HassEvent>();
+            var cache = new EntityStateCache(Mock.Of<IHassClient>(), testSubject);
+            var handlerCalled = false;
+
+            var changedEventData = new HassStateChangedEventData()
+            {
+                EntityId = "sensor.test",
+                OldState = new HassState(),
+                NewState = new HassState()
+                {
+                    State = "newState"
+                        
+                } 
+            };
+            
+            cache.StateAllChanges.Subscribe(e =>
+            {
+                // Verify that in the event handler the cache is already updated
+                Assert.Equal("newState", cache.GetState("sensor.test")?.State);
+                Assert.Equal(changedEventData, e);
+                handlerCalled = true;
+            });
+
+            // Act
+            testSubject.OnNext(new HassEvent {
+                Data = changedEventData 
+            });
+            
+            // Assert
+            Assert.True(handlerCalled);
+        }
+    }
+}
--- src/Model3/NetDeamon.Model3/Internal/EntityStateCache.cs
+++ src/Model3/NetDeamon.Model3/Internal/EntityStateCache.cs
@@ -1,23 +1,38 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
+using System.Reactive.Subjects;
 using JoySoftware.HomeAssistant.Client;
 using JoySoftware.HomeAssistant.Model;
 
 namespace NetDaemon.Model3.Internal
 {
     [SuppressMessage("", "CA1812", Justification = "Is Loaded via DependencyInjection")]
-    internal class EntityStateCache
+    internal class EntityStateCache : IDisposable
     {
         private readonly IHassClient _hassClient;
-
-        public EntityStateCache(IHassClient hassClient)
-        {
-            _hassClient = hassClient;
-        }
-
+        private readonly Subject<HassStateChangedEventData> _innerSubject = new();
+        private readonly IDisposable _eventSubscription;
         private readonly ConcurrentDictionary<string, HassState?> _latestStates = new();
 
-        public void Store(HassState newState) => _latestStates[newState.EntityId] = newState;
+        public EntityStateCache(IHassClient hassClient, IObservable<HassEvent> events)
+        {
+            _hassClient = hassClient;
+
+            _eventSubscription = events.Subscribe(HandleEvent);
+        }
+
+        private void HandleEvent(HassEvent hassEvent)
+        {
+            if (hassEvent.Data is not HassStateChangedEventData hassStateChangedEventData) return;
+            
+            // Make sure to first add the new state to the cache before calling other subscribers.
+            _latestStates[hassStateChangedEventData.EntityId] = hassStateChangedEventData.NewState;
+
+            _innerSubject.OnNext(hassStateChangedEventData);
+        }
+
+        public IObservable<HassStateChangedEventData> StateAllChanges => _innerSubject;
 
         public HassState? GetState(string entityId)
         {
@@ -25,6 +40,12 @@ namespace NetDaemon.Model3.Internal
             // this is a blocking call if it is not present but we want to avoid making GetState async
 
             return _latestStates.GetOrAdd(entityId, _ => _hassClient.GetState(entityId).Result);
+        }
+
+        public void Dispose()
+        {
+            _innerSubject.Dispose();
+            _eventSubscription.Dispose();
         }
     }
 }
