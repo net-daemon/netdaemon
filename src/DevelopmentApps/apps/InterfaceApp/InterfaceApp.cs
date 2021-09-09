7ea5e9dabf8b1617f36f901b6ffdb2de7c9e6d24
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-09-09 17:21:25 +0200

    Fix wrong DI setup order (#461)

--- src/DevelopmentApps/apps/InterfaceApp/InterfaceApp.cs
+++ src/DevelopmentApps/apps/InterfaceApp/InterfaceApp.cs
@@ -11,19 +11,16 @@ namespace NetDaemon.DevelopmentApps.apps.DebugApp
     public class InterfaceApp : IAsyncInitializable
     {
         private INetDaemon _host;
-        private readonly ILogger _logger;
 
-        public InterfaceApp(INetDaemonHost host, ILogger logger)
+        public InterfaceApp(INetDaemonHost host)
         {
             _host = host;
-            _logger = logger;
             _host.CallService("notify", "persistent_notification", new { message = "Hello", title = "Yay it works via DI! via Constructor" }, true);;
         }
 
         public Task InitializeAsync()
         {
             _host.CallService("notify", "persistent_notification", new { message = "Hello", title = "Yay it works via DI!" }, true);;
-            _logger.LogInformation("Logging via injected logger");
             return Task.CompletedTask;
         }
     }
