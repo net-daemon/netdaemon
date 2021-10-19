8b0f86ddaffbf8eae5668e4073ce3747011a873e
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-10-19 13:35:43 +0200

    Events on ha context (#478)
    * Added Events to IHaContext
    
    * Remove unused variable
    
    * Fix comment

--- src/Model3/NetDeamon.Model3/Common/IHaContext.cs
+++ src/Model3/NetDeamon.Model3/Common/IHaContext.cs
@@ -11,20 +11,25 @@ namespace NetDaemon.Model3.Common
     public interface IHaContext
     {
         /// <summary>
-        ///     The observable statestream, all changes including attributes
+        /// All Events from Home Assistant 
+        /// </summary>
+        IObservable<Event> Events { get; }
+
+        /// <summary>
+        ///     The observable state stream, all changes including attributes
         /// </summary>
         IObservable<StateChange> StateAllChanges { get; }
 
         /// <summary>
-        ///     The observable statestream state change
+        /// The observable state stream state change
         /// </summary>
         /// <remarks>
-        ///     Old state != New state
+        /// Old state != New state
         /// </remarks>
         IObservable<StateChange> StateChanges => StateAllChanges.Where(e => e.New?.State != e.Old?.State);
 
         /// <summary>
-        ///     Get state for a single entity
+        /// Get state for a single entity
         /// </summary>
         /// <param name="entityId"></param>
         EntityState? GetState(string entityId);
@@ -35,12 +40,12 @@ namespace NetDaemon.Model3.Common
         IReadOnlyList<Entity> GetAllEntities();
 
         /// <summary>
-        ///     Calls service in Home Assistant
+        /// Calls a service in Home Assistant
         /// </summary>
         /// <param name="domain">Domain of service</param>
         /// <param name="service">Service name</param>
         /// <param name="target">The target that is targeted by this service call</param>
-        /// <param name="data">Data provided to service. Use anonyomous type</param>
+        /// <param name="data">Data provided to service. Should be Json-serializable to the data expected by the service</param>
         void CallService(string domain, string service, ServiceTarget? target = null, object? data = null);
     }
 }
