1d6ba2423785108db76b612cbd04de127b00c194
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-10-19 14:31:07 +0200

    CodeGen CLI (#482)
    * Implemented code generation CLI
    
    * Fixes
    
    * disable warning
    
    * Update NetDaemon.Model3.CodeGenerator.csproj
    
    Removed empty folder

--- src/Model3/NetDaemon.Model3.CodeGenerator/Generator.Entitites.cs
+++ src/Model3/NetDaemon.Model3.CodeGenerator/Generator.Entitites.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using NetDaemon.Daemon.Config;
 using NetDaemon.Model3.CodeGenerator.Extensions;
 using NetDaemon.Model3.CodeGenerator.Helpers;
 using NetDaemon.Model3.Common;
