53a353cb4de27e0c05a6ce612e0d1fdf039658c2
Tomas Hellström <tomas.hellstrom@yahoo.se>
2021-11-27 18:13:02 +0100

    Adds application context for apps

---
+++ src/App/NetDaemon.App/Common/IApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NetDaemon.Common
+{
+    /// <summary>
+    /// Provides application runtime data to apps
+    /// </summary>
+    public interface IApplicationContext
+    {
+        /// <summary>
+        ///     Unique id of the application
+        /// </summary>
+        string? Id { get; }
+
+        /// <summary>
+        ///     Gets whether this app is enabled.
+        /// </summary>
+        bool IsEnabled { get; }
+
+        /// <summary>
+        ///     Unique id of the application entity
+        /// </summary>
+        string EntityId => $"switch.netdaemon_{Id?.ToSafeHomeAssistantEntityId()}";
+    }
+}
