285d42b8d3ee7ea366dbfea6465fa5b25136619d
Eugene Niemand <eugene.niemand@gmail.com>
2021-12-15 15:46:29 +0000

    adding XML Comments

--- src/HassModel/NetDeamon.HassModel/Entities/StateChange.cs
+++ src/HassModel/NetDeamon.HassModel/Entities/StateChange.cs
@@ -1,46 +1,58 @@
-﻿namespace NetDaemon.HassModel.Entities
+﻿namespace NetDaemon.HassModel.Entities;
+
+/// <summary>
+/// Represents a state change event for an entity
+/// </summary>
+public record StateChange
 {
     /// <summary>
-    /// Represents a state change event for an entity
+    /// This should not be used under normal circumstances but can be used for unit testing of apps
     /// </summary>
-    public record StateChange
+    /// <param name="entity"></param>
+    /// <param name="old"></param>
+    /// <param name="new"></param>
+    public StateChange(Entity entity, EntityState? old, EntityState? @new)
     {
-        public StateChange(Entity entity, EntityState? old, EntityState? @new) 
-        {
-            Entity = entity;
-            New = @new;
-            Old = old;
-        }
-
-        /// <summary>The Entity that changed</summary>
-        public virtual Entity Entity { get; } = default!; // Somehow this is needed to avoid a warning about this field being initialized
-
-        /// <summary>The old state of the entity</summary>
-        public virtual EntityState? Old { get; }
-
-        /// <summary>The new state of the entity</summary>
-        public virtual EntityState? New { get; }
+        Entity = entity;
+        New    = @new;
+        Old    = old;
     }
 
+    /// <summary>The Entity that changed</summary>
+    public virtual Entity Entity { get; } = default!; // Somehow this is needed to avoid a warning about this field being initialized
+
+    /// <summary>The old state of the entity</summary>
+    public virtual EntityState? Old { get; }
+
+    /// <summary>The new state of the entity</summary>
+    public virtual EntityState? New { get; }
+}
+
+/// <summary>
+/// Represents a state change event for a strong typed entity and state 
+/// </summary>
+/// <typeparam name="TEntity">The Type</typeparam>
+/// <typeparam name="TEntityState"></typeparam>
+public record StateChange<TEntity, TEntityState> : StateChange
+    where TEntity : Entity
+    where TEntityState : EntityState
+{
     /// <summary>
-    /// Represents a state change event for a strong typed entity and state 
+    /// This should not be used under normal circumstances but can be used for unit testing of apps
     /// </summary>
-    /// <typeparam name="TEntity">The Type</typeparam>
-    /// <typeparam name="TEntityState"></typeparam>
-    public record StateChange<TEntity, TEntityState> : StateChange 
-        where TEntity : Entity 
-        where TEntityState : EntityState
+    /// <param name="entity"></param>
+    /// <param name="old"></param>
+    /// <param name="new"></param>
+    public StateChange(TEntity entity, TEntityState? old, TEntityState? @new) : base(entity, old, @new)
     {
-        public StateChange(TEntity entity, TEntityState? old, TEntityState? @new) : base(entity, old, @new)
-        { }
-
-        /// <inheritdoc/>
-        public override TEntity Entity => (TEntity)base.Entity;
-
-        /// <inheritdoc/>
-        public override TEntityState? New => (TEntityState?)base.New;
-
-        /// <inheritdoc/>
-        public override TEntityState? Old => (TEntityState?)base.Old;
     }
+
+    /// <inheritdoc/>
+    public override TEntity Entity => (TEntity)base.Entity;
+
+    /// <inheritdoc/>
+    public override TEntityState? New => (TEntityState?)base.New;
+
+    /// <inheritdoc/>
+    public override TEntityState? Old => (TEntityState?)base.Old;
 }
