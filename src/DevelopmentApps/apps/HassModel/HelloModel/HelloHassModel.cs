5a2126197615ae2e63ab80d51aafa015d8d9e2f6
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-12-16 05:08:33 +0100

    Add SendEvent to HassModel (#553)
    * Add SendEvent to HassModel

--- src/DevelopmentApps/apps/HassModel/HelloModel/HelloHassModel.cs
+++ src/DevelopmentApps/apps/HassModel/HelloModel/HelloHassModel.cs
@@ -1,21 +1,15 @@
-using System;
 using NetDaemon.Common;
 using NetDaemon.Extensions.Scheduler;
 using NetDaemon.HassModel.Common;
-using NetDaemon.HassModel.Integration;
 
 namespace NetDaemon.DevelopmentApps.apps.M3Test
 {
     [NetDaemonApp]
-    [Focus]
     public class HelloNewModelApp
     {
         public HelloNewModelApp(IHaContext ha, INetDaemonScheduler scheduler)
         {
-            ha.RegisterServiceCallBack("CallMe3",  (ServiceData e) => Console.WriteLine($"{e?.name} + {e?.value}"));
+            ha.SendEvent("test", new { name="Frank", age = 45});
         }
     }
-
-    record ServiceData(string? name, int value);
-
 }
