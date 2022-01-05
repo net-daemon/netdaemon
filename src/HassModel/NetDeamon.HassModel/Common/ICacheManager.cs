db26e9797df5b809a0fd130d5967ed200f93614b
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2022-01-05 06:19:28 +0100

    Added HassModel CacheManager

---
+++ src/HassModel/NetDeamon.HassModel/Common/ICacheManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetDaemon.HassModel.Common;
+
+/// <summary>
+/// Allows initialization and refreshing of the HassModel internal caches
+/// </summary>
+public interface ICacheManager
+{
+    /// <summary>
+    /// (re) Initializes the Hass Model internal caches from Home Assistant. Should be called
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task Initialize(CancellationToken cancellationToken);
+}
---
+++ src/HassModel/NetDeamon.HassModel/Internal/Client/CacheManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NetDaemon.HassModel.Common;
+
+namespace NetDaemon.HassModel.Internal.Client;
+
+internal class CacheManager : ICacheManager
+{
+    private readonly EntityAreaCache _entityAreaCache;
+    private readonly EntityStateCache _entityStateCache;
+
+    public CacheManager(EntityAreaCache entityAreaCache, EntityStateCache entityStateCache)
+    {
+        _entityAreaCache = entityAreaCache;
+        _entityStateCache = entityStateCache;
+    }
+
+    public async Task Initialize(CancellationToken cancellationToken)
+    {
+        await _entityAreaCache.InitializeAsync(cancellationToken).ConfigureAwait(false);
+        await _entityStateCache.InitializeAsync(cancellationToken).ConfigureAwait(false);
+    }
+}
