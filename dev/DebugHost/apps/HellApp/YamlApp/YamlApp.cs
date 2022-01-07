61eb6a005f753d7b2293f6865aad33f0bf75c9a6
Tomas Hellström <tomas.hellstrom@yahoo.se>
2022-01-08 00:10:52 +0100

    Re-add development apps, the new way (#584)

---
+++ dev/DebugHost/apps/HellApp/YamlApp/YamlApp.cs
@@ -0,0 +1,24 @@
+
+using NetDaemon.AppModel;
+using NetDaemon.HassModel.Common;
+
+namespace Apps;
+
+[NetDaemonApp]
+public class YamlApp
+{
+    public YamlApp(IHaContext ha, IAppConfig<TestConfig> config)
+    {
+        ha?.CallService("notify", "persistent_notification", data: new
+        {
+            message = $"My sensor is {config?.Value.MySensor}",
+            title = "Hello config!"
+        });
+    }
+}
+
+// Todo: Make this a real HassModel entity
+public record TestConfig
+{
+    public string? MySensor { get; set; }
+}
