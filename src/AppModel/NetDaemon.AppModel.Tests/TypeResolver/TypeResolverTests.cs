a6772ff503b535e2afaa74e577df09a3cd70ef6a
Tomas Hellström <tomas.hellstrom@yahoo.se>
2022-01-15 16:04:55 +0100

    Add test for disposal of AssemblyResolver

--- src/AppModel/NetDaemon.AppModel.Tests/TypeResolver/TypeResolverTests.cs
+++ src/AppModel/NetDaemon.AppModel.Tests/TypeResolver/TypeResolverTests.cs
@@ -147,6 +147,68 @@ public class TypeResolverTests
     }
 
     [Fact]
+    public void AssemblyResolverShouldDisposeWithoutError()
+    {
+        var syntaxTreeResolverMock = new Mock<ISyntaxTreeResolver>();
+        // We setup the mock to return a prebuilt syntax tree with a fake class
+        syntaxTreeResolverMock
+            .Setup(
+                n => n.GetSyntaxTrees()
+            )
+            .Returns(
+                () =>
+                {
+                    var result = new List<SyntaxTree>();
+                    var sourceText = SourceText.From(
+                        @"
+                        public class FakeClass
+                        {
+                            
+                        }
+                        "
+                        , Encoding.UTF8);
+                    var syntaxTree = SyntaxFactory.ParseSyntaxTree(sourceText, path: "fakepath.cs")
+                                     ?? throw new NullReferenceException("unexpected null reference");
+
+                    result.Add(
+                        syntaxTree
+                    );
+                    return result;
+                }
+            );
+
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddLogging();
+        serviceCollection.AddTransient<IOptions<AppConfigurationLocationSetting>>(
+            _ => new FakeOptions(Path.Combine(AppContext.BaseDirectory,
+                Path.Combine(AppContext.BaseDirectory, "Fixtures/Dynamic"))));
+        serviceCollection.AddSingleton(_ => syntaxTreeResolverMock.Object);
+        serviceCollection.AddAppModel();
+        serviceCollection.AddAppTypeResolver();
+        serviceCollection.AddSingleton<CompilerFactory>();
+        serviceCollection.AddSingleton<ICompilerFactory>(s => s.GetRequiredService<CompilerFactory>());
+        serviceCollection.AddSingleton<DynamicallyCompiledAssemblyResolver>();
+        serviceCollection.AddSingleton<IAssemblyResolver>(s =>
+            s.GetRequiredService<DynamicallyCompiledAssemblyResolver>());
+
+
+        var provider = serviceCollection.BuildServiceProvider();
+
+        var typeResolver = provider.GetService<IAppTypeResolver>() ??
+                           throw new NullReferenceException("Not expected null");
+
+        var t = typeResolver.GetTypes().Where(n => n.Name == "FakeClass").ToList();
+        t.Should().HaveCount(1);
+
+        var assemblyResolver = provider.GetRequiredService<DynamicallyCompiledAssemblyResolver>();
+
+        // Assert Dispose does not throw
+        var exception = Record.Exception(() => assemblyResolver.Dispose());
+        Assert.Null(exception);
+    }
+
+    [Fact]
     public void TestAddAppFromTypeShouldLoadSigleApp()
     {
         var serviceCollection = new ServiceCollection();
