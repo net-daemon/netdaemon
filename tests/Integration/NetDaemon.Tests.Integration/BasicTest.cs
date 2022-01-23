1f9b2d078cd6fc2cf2d101a00fe3156147fa3b08
Tomas Hellström <tomas.hellstrom@yahoo.se>
2022-01-23 20:09:12 +0100

    Add integration tests.

---
+++ tests/Integration/NetDaemon.Tests.Integration/BasicTest.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NetDaemon.AppModel;
+using NetDaemon.HassModel.Common;
+using NetDaemon.HassModel.Entities;
+using NetDaemon.Tests.Integration.Helpers;
+using Xunit;
+
+namespace NetDaemon.Tests.Integration;
+
+[NetDaemonApp]
+public class BasicApp
+{
+    public BasicApp(
+        IHaContext haContext
+    )
+    {
+        haContext.StateChanges()
+            .Where(n =>
+                n.Entity.EntityId == "input_select.who_cooks"
+            )
+            .Subscribe(
+                s => haContext.CallService("input_text", "set_value",
+                    ServiceTarget.FromEntities("input_text.test_result"), new {value = s.New?.State})
+            );
+    }
+}
+
+public class BasicTests : IClassFixture<MakeSureNetDaemonIsRunningFixture>
+{
+    private readonly IHaContext _haContext;
+
+    public BasicTests(
+        MakeSureNetDaemonIsRunningFixture _,
+        IHaContext haContext
+    )
+    {
+        _haContext = haContext;
+    }
+
+    [Fact]
+    public async Task BasicTestApp_ShouldChangeStateOfInputTextToTheStateOfInputSelect_WhenChange()
+    {
+        var optionToSet = GetDifferentOptionThanCurrentlySelected();
+
+        var waitTask = _haContext.StateChanges()
+            .Where(n => n.Entity.EntityId == "input_text.test_result")
+            .Timeout(TimeSpan.FromMilliseconds(5000))
+            .FirstAsync()
+            .ToTask();
+
+        _haContext.CallService(
+            "input_select",
+            "select_option",
+            ServiceTarget.FromEntities("input_select.who_cooks"),
+            new {option = optionToSet});
+
+        var result = await waitTask.ConfigureAwait(false);
+
+        result.New!.State.Should().Be(optionToSet);
+    }
+
+    private string GetDifferentOptionThanCurrentlySelected()
+    {
+        var currentState = _haContext.GetState("input_select.who_cooks")?.State
+                           ?? throw new InvalidOperationException();
+
+        var useOption = currentState switch
+        {
+            "Paulus" => "Anne Therese",
+            _ => "Paulus"
+        };
+        return useOption;
+    }
+}
---
+++ tests/Integration/NetDaemon.Tests.Integration/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Hosting;
+using NetDaemon.AppModel;
+using NetDaemon.Runtime;
+
+namespace NetDaemon.Tests.Integration;
+
+/// <summary>
+///     Startup class
+/// </summary>
+/// <remarks>
+///     XUnit.DependencyInjection logic finds the startup here and we can make our custom
+///     host builder that allows dependency injection in test classes
+/// </remarks>
+public class Startup
+{
+    public IHostBuilder CreateHostBuilder(AssemblyName assemblyName)
+    {
+        return Host.CreateDefaultBuilder()
+            .UseNetDaemonAppSettings()
+            .UseNetDaemonRuntime()
+            .ConfigureServices((_, services) =>
+                services
+                    .AddAppsFromAssembly(Assembly.GetExecutingAssembly())
+                    .AddNetDaemonStateManager()
+            );
+    }
+}
