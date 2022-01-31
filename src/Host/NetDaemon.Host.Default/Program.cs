567930832fc415594ee0e654cc27bd3830bac994
Tomas Hellström <tomas.hellstrom@yahoo.se>
2022-01-31 07:23:17 +0100

    Loads HassModel.Integration the proper way (#646)
    * Fixes loading of HassModel.Intergration the proper way.
    
    * Review fix

--- src/Host/NetDaemon.Host.Default/Program.cs
+++ src/Host/NetDaemon.Host.Default/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
 using Microsoft.Extensions.Hosting;
 using NetDaemon.AppModel;
 using NetDaemon.Extensions.Logging;
 using NetDaemon.Extensions.Scheduler;
 using NetDaemon.Extensions.Tts;
 using NetDaemon.Runtime;
-
+using NetDaemon.HassModel.Integration;
 #pragma warning disable CA1812
+
+// This assembly is not used by the host so we have to force load it
+// so it will be available for source deployment scenarios
+_ = AssemblyLoadContext.Default.LoadFromAssemblyPath(typeof(IntegrationHaContextExtensions).Assembly.Location);
 
 try
 {
