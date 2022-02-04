4fc6954434e4986cb11b15b14475ce07be164bc1
Eugene Niemand <eugene.niemand@gmail.com>
2022-02-04 08:37:31 +0000

    Moved to src/extensions, renamed folder and fixed warning

--- src/Extensions/NetDaemon.Extensions.MqttEntityManager/MqttConfiguration.cs
+++ src/Extensions/NetDaemon.Extensions.MqttEntityManager/MqttConfiguration.cs
@@ -2,7 +2,7 @@
 
 internal class MqttConfiguration
 {
-    public string Host { get; set; }
+    public string Host { get; set; } = string.Empty;
     public int Port { get; set; } = 1883;
     public string? UserId { get; set; }
     public string? Password { get; set; }
