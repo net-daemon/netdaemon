b474729f128477b790e3d8905530182f4a09d552
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2022-02-05 23:53:06 +0100

    Undo changes for other PR

--- src/HassModel/NetDeamon.HassModel/Entities/EntityExtensions.cs
+++ src/HassModel/NetDeamon.HassModel/Entities/EntityExtensions.cs
@@ -56,21 +56,4 @@ public static class EntityExtensions
 
     internal static IObservable<T> StateChangesOnly<T>(this IObservable<T> changes) where T : StateChange
         => changes.Where(c => c.New?.State != c.Old?.State);
-    
-    public static IObservable<StateChange> SameStateFor(this IObservable<StateChange> observable, Func<EntityState?, bool> predicate, TimeSpan timeSpan) =>
-        observable
-            .Where(e => predicate(e.Old) != predicate(e.New))
-            .Throttle(timeSpan)
-            .Where(e => predicate(e.New));
-    
-    /// <summary>
-    /// Observable, All state changes. New.State != Old.State
-    /// </summary>
-    public static IObservable<StateChange<TEntity, TEntityState>> SameStateFor<TEntity, TEntityState>(this IObservable<StateChange<TEntity, TEntityState>> observable, Func<TEntityState?, bool> predicate, TimeSpan timeSpan) 
-        where TEntity : Entity
-        where TEntityState : EntityState =>
-        observable
-            .Where(e => predicate(e.Old) != predicate(e.New))
-            .Throttle(timeSpan)
-            .Where(e => predicate(e.New));
 }
