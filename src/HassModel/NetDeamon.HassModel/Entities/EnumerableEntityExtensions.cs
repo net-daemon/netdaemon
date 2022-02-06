e31839b45d94dfde50320a730eeb9295897abbe6
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2022-02-06 17:53:58 +0100

    Added ExtensionMethods to codegen for calling service methods in IEnumerable<Entity> (#661)
    * Added ExtensionMethods to codegen for calling service methods in IEnumerable<Entity>
    
    * Undo changes for other PR
    
    * Disalow calling diferent domains in one call

--- src/HassModel/NetDeamon.HassModel/Entities/EnumerableEntityExtensions.cs
+++ src/HassModel/NetDeamon.HassModel/Entities/EnumerableEntityExtensions.cs
@@ -34,19 +34,38 @@ public static class EnumerableEntityExtensions
         where TEntityState : EntityState<TAttributes>
         where TAttributes : class => 
         entities.StateAllChanges().StateChangesOnly();
-        
+
     /// <summary>
     /// Calls a service with a set of Entities as the target
     /// </summary>
-    public static void CallService(this IEnumerable<Entity> entities, string domain, string service, object? data)
+    /// <param name="entities">IEnumerable of Entities for which to call the service</param>
+    /// <param name="service">Name of the service to call. If the Domain of the service is the same as the domain of the Entities it can be omitted</param>
+    /// <param name="data">Data to provide</param>
+    public static void CallService(this IEnumerable<Entity> entities, string service, object? data = null)
     {
-        // Usually each Entity will have the same IHaContext, but just in case group by the context and call the
-        // service for each context separately
-            
-        var perContext = entities.GroupBy(e => e.HaContext);
-        foreach (var group in perContext)
+        ArgumentNullException.ThrowIfNull(service);
+        
+        entities = entities.ToList();
+        
+        if (!entities.Any()) return;
+        
+        var (serviceDomain, serviceName) = service.SplitAtDot();
+
+        if (serviceDomain == null)
         {
-            group.Key.CallService(domain, service, new ServiceTarget { EntityIds = group.Select(e => e.EntityId).ToList() }, data);
+            var domainsFromEntity = entities.Select(e => e.EntityId.SplitAtDot().Left).Distinct().Take(2).ToArray();
+            if (domainsFromEntity.Length != 1) throw new InvalidOperationException($"Cannot call service {service} for entities that do not have the same domain");
+            
+            serviceDomain = domainsFromEntity.First()!;
+        }
+        
+        // Usually each Entity will have the same IHaContext and domain, but just in case its not, group by the context and domain and call the
+        // service for each group separately
+        var serviceCalls = entities.GroupBy(e => e.HaContext);
+        
+        foreach (var group in serviceCalls)
+        {
+            group.Key.CallService(serviceDomain, serviceName, new ServiceTarget { EntityIds = group.Select(e => e.EntityId).ToList() }, data);
         }
     }
 }
