6bf2b2f6cbb8128b75cf123a71e1e508fc7444c6
Scott Leckie <scott.leckie@kwolo.com>
2022-02-07 18:40:24 +0000

    Managed mqtt client, updated mqtt test app (#667)

--- dev/DebugHost/apps/Extensions/MqttEntityManagerApp.cs
+++ dev/DebugHost/apps/Extensions/MqttEntityManagerApp.cs
@@ -20,6 +20,7 @@ public class MqttEntityManagerApp : IAsyncInitializable
     private readonly IHaContext _ha;
     private readonly ILogger<MqttEntityManagerApp> _logger;
     private readonly IMqttEntityManager _entityManager;
+    private Task? _exerciseTask;
 
     public MqttEntityManagerApp(IHaContext ha, ILogger<MqttEntityManagerApp> logger, IMqttEntityManager entityManager)
     {
@@ -28,19 +29,19 @@ public class MqttEntityManagerApp : IAsyncInitializable
         _entityManager = entityManager;
     }
 
-    [SuppressMessage("Naming", "CA1727:Use PascalCase for named placeholders", Justification = "<Pending>")]
     [SuppressMessage("Design", "CA1031:Do not catch general exception types",
         Justification = "We need to log unexpected errors")]
+#pragma warning disable 1998    // We may be debugging here, so don't block the initialize function
     public async Task InitializeAsync(CancellationToken cancellationToken)
+    {
+        _exerciseTask = Task.Run(() => ExercisorAsync(cancellationToken), cancellationToken);
+    }
+#pragma warning disable 1998
+
+    private async Task ExercisorAsync(CancellationToken cancellationToken)
     {
         try
         {
-            await _entityManager.RemoveAsync("binary_sensor.mqtt_test").ConfigureAwait(false);
-            await _entityManager.RemoveAsync("binary_sensor.nd_test").ConfigureAwait(false);
-            await _entityManager.RemoveAsync("binary_sensor.leith_tides").ConfigureAwait(false);
-            await _entityManager.RemoveAsync("input_text.sample_text_helper").ConfigureAwait(false);
-
-
             // Basic entity creation
             await _entityManager.CreateAsync("sensor.basic_sensor").ConfigureAwait(false);
 
@@ -59,7 +60,7 @@ public class MqttEntityManagerApp : IAsyncInitializable
                 .ConfigureAwait(false);
 
             // Walk through a more complete set of examples, checking HA to verify that each operation completed
-            _logger.LogInformation("Creating Entity {domain}.{entityId}", "binary_sensor", "manager_test");
+            _logger.LogInformation("Creating Entity binary_sensor.manager_test");
             var createOptions = new EntityCreationOptions(DeviceClass: "motion", Name: "Manager Test");
             await _entityManager.CreateAsync("binary_sensor.manager_test", createOptions).ConfigureAwait(false);
             // Using Delay to give Mqtt and HA enough time to process events.
@@ -67,19 +68,18 @@ public class MqttEntityManagerApp : IAsyncInitializable
             await Task.Delay(250, cancellationToken).ConfigureAwait(false);
 
             var entity = _ha.Entity("binary_sensor.manager_test");
-            _logger.LogInformation("Entity {domain}.{entityId} State: {state}", "binary_sensor", "manager_test",
-                entity.State);
+            _logger.LogInformation("Entity binary_sensor.manager_test State: {State}", entity.State);
 
             await _entityManager.UpdateAsync("binary_sensor.manager_test", "ON", new { attribute1 = "attr1" })
                 .ConfigureAwait(false);
             await Task.Delay(250, cancellationToken).ConfigureAwait(false);
-            _logger.LogInformation("Entity {domain}.{entityId} State: {state} Attributes: {attributes}",
-                "binary_sensor", "manager_test", entity.State, entity.Attributes);
+            _logger.LogInformation("Entity binary_sensor.manager_test State: {State} Attributes: {Attributes}",
+                entity.State, entity.Attributes);
 
             await _entityManager.RemoveAsync("binary_sensor.manager_test").ConfigureAwait(false);
             await Task.Delay(250, cancellationToken).ConfigureAwait(false);
             var removed = _ha.Entity("binary_sensor.manager_test").State == null;
-            _logger.LogInformation("Removed Entity: {removed}", removed);
+            _logger.LogInformation("Removed Entity: {Removed}", removed);
 
             // Remove other entities
             await _entityManager.RemoveAsync("sensor.basic_sensor").ConfigureAwait(false);
@@ -89,6 +89,7 @@ public class MqttEntityManagerApp : IAsyncInitializable
         catch (Exception e)
         {
             _logger.LogError(e, e.Message);
+            throw;
         }
     }
 }
---
+++ src/Extensions/NetDaemon.Extensions.MqttEntityManager/AssuredMqttConnection.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using NetDaemon.Extensions.MqttEntityManager.Exceptions;
+
+namespace NetDaemon.Extensions.MqttEntityManager;
+
+/// <summary>
+/// Wrapper to assure an MQTT connection
+/// </summary>
+internal class AssuredMqttConnection : IAssuredMqttConnection, IDisposable
+{
+    private const int RetryMaximumSeconds = 15;
+    private readonly ILogger<AssuredMqttConnection> _logger;
+    private readonly Task _connectionTask;
+    private IManagedMqttClient? _mqttClient;
+    private bool _disposed;
+
+    /// <summary>
+    /// Wrapper to assure an MQTT connection
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="mqttFactory"></param>
+    /// <param name="mqttConfig"></param>
+    public AssuredMqttConnection(
+        ILogger<AssuredMqttConnection> logger,
+        IMqttFactory mqttFactory,
+        IOptions<MqttConfiguration> mqttConfig)
+    {
+        _logger = logger;
+
+        _logger.LogDebug("MQTT initiating connection");
+        _connectionTask = Task.Run(() => ConnectAsync(mqttConfig.Value, mqttFactory));
+    }
+
+    /// <summary>
+    /// Ensures that the MQTT client is available
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="MqttConnectionException">Timed out while waiting for connection</exception>
+    public async Task<IManagedMqttClient> GetClientAsync()
+    {
+        await _connectionTask;
+
+        return _mqttClient ?? throw new MqttConnectionException("Unable to create MQTT connection");
+    }
+
+    private async Task ConnectAsync(MqttConfiguration mqttConfig, IMqttFactory mqttFactory)
+    {
+        _logger.LogDebug("Connecting to MQTT broker at {Host}:{Port}/{UserName}", 
+            mqttConfig.Host, mqttConfig.Port, mqttConfig.UserName);
+
+        var clientOptions = new ManagedMqttClientOptionsBuilder()
+            .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+            .WithClientOptions(new MqttClientOptionsBuilder()
+                .WithTcpServer(mqttConfig.Host, mqttConfig.Port)
+                .WithCredentials(mqttConfig.UserName, mqttConfig.Password))
+            .Build();
+
+        _mqttClient = mqttFactory.CreateManagedMqttClient();
+
+        _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(LogConnectedStatus);
+        _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(LogDisconnectedStatus);
+
+        await _mqttClient.StartAsync(clientOptions);
+        
+        _logger.LogDebug("MQTT client is ready");
+    }
+
+    private void LogDisconnectedStatus(MqttClientDisconnectedEventArgs arg)
+    {
+        _logger.LogDebug("MQTT disconnected: {Reason}", arg.Reason);
+    }
+
+    private void LogConnectedStatus(MqttClientConnectedEventArgs arg)
+    {
+        _logger.LogDebug("MQTT connected: {ResultCode}", arg.ConnectResult.ResultCode);
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+            return;
+
+        _disposed = true;
+        _logger.LogInformation("MQTT disconnecting");
+        _connectionTask?.Dispose();
+        _mqttClient?.Dispose();
+    }
+}
