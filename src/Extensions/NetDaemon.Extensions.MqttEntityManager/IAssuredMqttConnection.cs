ec3e9f6a4ed5cf7dc4229ee6cc55ba673bbbd7fb
Scott Leckie <scott.leckie@kwolo.com>
2022-02-07 18:35:19 +0000

    Managed mqtt client, updated mqtt test app

---
+++ src/Extensions/NetDaemon.Extensions.MqttEntityManager/IAssuredMqttConnection.cs
@@ -0,0 +1,14 @@
+﻿using MQTTnet.Extensions.ManagedClient;
+
+namespace NetDaemon.Extensions.MqttEntityManager;
+
+/// <summary>
+/// Wrapper to assure an MQTT connection
+/// </summary>
+internal interface IAssuredMqttConnection
+{
+    /// <summary>
+    /// Ensures that the MQTT client is available
+    /// </summary>
+    Task<IManagedMqttClient> GetClientAsync();
+}
