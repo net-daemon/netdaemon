5e053aebc156524f427dfdc4abb3d7e5b9119e97
Tomas Hellström <tomas.hellstrom@yahoo.se>
2022-02-10 20:53:44 +0100

    Fix tests

--- src/Client/NetDaemon.HassClient.Tests/HelperTest/ResultMessageHandlerTests.cs
+++ src/Client/NetDaemon.HassClient.Tests/HelperTest/ResultMessageHandlerTests.cs
@@ -28,19 +28,24 @@ public class ResultMessageHandlerTests
 
 
     [Fact]
-    public async Task TestTaskCompleteWithErrorResultShouldLogError()
+    public async Task TestTaskCompleteWithErrorResultShouldLogWarning()
     {
         var task = SomeUnSuccessfulResult();
         _resultMessageHandler.HandleResult(task, new CommandMessage {Type = "test"});
         await _resultMessageHandler.DisposeAsync().ConfigureAwait(false);
         task.IsCompletedSuccessfully.Should().BeTrue();
-        _loggerMock.Verify(
-            x => x.Log(
-                It.Is<LogLevel>(n => n == LogLevel.Warning),
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((_, __) => true),
-                It.IsAny<Exception>(),
-                It.Is<Func<It.IsAnyType, Exception, string>>((_, _) => true)!), Times.Once());
+        _loggerMock.VerifyWarningWasCalled("Failed command (test) error: (null).  Sent command is CommandMessage { Type = test, Id = 0 }");
+    }
+
+    [Fact]
+    public async Task TestTaskCompleteWithExceptionShouldLogError()
+    {
+        var task = SomeUnSuccessfulResultThrowsException();
+        _resultMessageHandler.HandleResult(task, new CommandMessage {Type = "test"});
+        await _resultMessageHandler.DisposeAsync().ConfigureAwait(false);
+        task.IsCompletedSuccessfully.Should().BeFalse();
+        task.IsFaulted.Should().BeTrue();
+        _loggerMock.VerifyErrorWasCalled("Exception waiting for result message  Sent command is CommandMessage { Type = test, Id = 0 }");
     }
 
     [Fact]
@@ -52,34 +57,10 @@ public class ResultMessageHandlerTests
         _resultMessageHandler.HandleResult(task, new CommandMessage {Type = "test"});
         await _resultMessageHandler.DisposeAsync().ConfigureAwait(false);
         task.IsCompletedSuccessfully.Should().BeTrue();
-
-        _loggerMock.Verify(
-            x => x.Log(
-                It.Is<LogLevel>(n => n == LogLevel.Warning),
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((_, __) => true),
-                It.IsAny<Exception>(),
-                It.Is<Func<It.IsAnyType, Exception, string>>((_, _) => true)!), Times.Once());
+        _loggerMock.VerifyWarningWasCalled("Command (test) did not get response in timely fashion.  Sent command is CommandMessage { Type = test, Id = 0 }");
     }
 
-    [Fact]
-    public async Task TestTaskCompleteWithTimeoutAndThenErrorShouldLogWarningTwice()
-    {
-        _resultMessageHandler.WaitForResultTimeout = 1;
 
-        var task = SomeUnSuccessfulResult();
-        _resultMessageHandler.HandleResult(task, new CommandMessage {Type = "test"});
-        await _resultMessageHandler.DisposeAsync().ConfigureAwait(false);
-        task.IsCompletedSuccessfully.Should().BeTrue();
-
-        _loggerMock.Verify(
-            x => x.Log(
-                It.Is<LogLevel>(n => n == LogLevel.Warning),
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((_, __) => true),
-                It.IsAny<Exception>(),
-                It.Is<Func<It.IsAnyType, Exception, string>>((_, _) => true)!), Times.Exactly(2));
-    }
 
     private async Task<HassMessage> SomeSuccessfulResult()
     {
@@ -93,5 +74,12 @@ public class ResultMessageHandlerTests
         // Simulate som time
         await Task.Delay(100);
         return new HassMessage {Success = false};
+    }
+
+    private async Task<HassMessage> SomeUnSuccessfulResultThrowsException()
+    {
+        // Simulate som time
+        await Task.Delay(100);
+        throw new InvalidOperationException("Ohh noooo!");
     }
 }
