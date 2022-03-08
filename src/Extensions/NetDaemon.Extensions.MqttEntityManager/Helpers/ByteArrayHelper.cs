938d7348ddeddf05414fd67070ada0aaf7fda9c2
Scott Leckie <scott.leckie@kwolo.com>
2022-03-08 21:54:04 +0000

    Safe method to decode byte array to string

---
+++ src/Extensions/NetDaemon.Extensions.MqttEntityManager/Helpers/ByteArrayHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace NetDaemon.Extensions.MqttEntityManager.Helpers;
+
+internal static class ByteArrayHelper
+{
+     public static string SafeToString(byte[]? array)
+     {
+          try
+          {
+               if (array == null || array.Length == 0)
+                    return "";
+               
+               return Encoding.UTF8.GetString(array);
+          }
+          catch (Exception e)
+          {
+               return "";
+          }
+     }
+}
