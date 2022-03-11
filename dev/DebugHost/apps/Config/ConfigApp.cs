388ee7ec83da724e6f07762facfd4dc6174761f1
Tomas Hellström <tomas.hellstrom@yahoo.se>
2022-03-11 08:19:06 +0100

    Throws exception using read only collection wrongly in configuration

---
+++ dev/DebugHost/apps/Config/ConfigApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using NetDaemon.AppModel;
+
+namespace Apps;
+
+[NetDaemonApp]
+// [Focus]
+public class ConfigApp
+{
+    public ConfigApp(IAppConfig<AppConfig> config)
+    {
+        Console.WriteLine(config.Value);
+    }
+}
+
+public record AppConfig
+{
+    public IList<string> Strings { get; init; } = new List<string>();
+}
