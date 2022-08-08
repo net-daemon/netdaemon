44b5e00261602efaa44351cb74223520e37613a8
Tomas Hellström <tomas.hellstrom@yahoo.se>
2022-08-08 09:58:39 +0200

    SubscribeSafe (#733)
    * SubscribeSafe added.
    
    * Added ILogger overload
    
    * Fixed check if logger exists before creating factory

--- src/HassModel/NetDaemon.HassModel.Tests/ObservableExtensionsTest.cs
+++ src/HassModel/NetDaemon.HassModel.Tests/ObservableExtensionsTest.cs
@@ -9,61 +9,111 @@ namespace NetDaemon.HassModel.Tests;
 
 public class ObservableExtensionsTest
 {
-
     [Fact]
     public void TestSubscribeAsyncShouldCallAsyncFunction()
     {
         var testSubject = new Subject<string>();
-        var observerMock = new Mock<IObserver<string>>();
         var str = string.Empty;
-        
-        testSubject.SubscribeAsync((a) =>
+
+        testSubject.SubscribeAsync(a =>
         {
             str = a;
             return Task.CompletedTask;
         });
-        
+
         testSubject.OnNext("hello");
         str.Should().Be("hello");
-    }    
-    
+    }
+
+    [Fact]
+    public void TestSubscribeSafeShouldCallFunction()
+    {
+        var testSubject = new Subject<string>();
+        var str = string.Empty;
+
+        testSubject.SubscribeSafe(a => { str = a; });
+
+        testSubject.OnNext("hello");
+        str.Should().Be("hello");
+    }
+
     [Fact]
     public void TestSubscribeAsyncConcurrentShouldCallAsyncFunction()
     {
         var testSubject = new Subject<string>();
         var observerMock = new Mock<IObserver<string>>();
         var str = string.Empty;
-        
-        testSubject.SubscribeAsyncConcurrent((a) =>
+
+        testSubject.SubscribeAsyncConcurrent(a =>
         {
             str = a;
             return Task.CompletedTask;
         });
-        
+
         testSubject.OnNext("hello");
         str.Should().Be("hello");
-    }    
-    
+    }
+
     [Fact]
     public void TestSubscribeAsyncExceptionShouldCallErrorCallback()
     {
         var testSubject = new Subject<string>();
-        bool errorCalled = false;
-        var subscriber = testSubject.SubscribeAsync((a) => throw new InvalidOperationException(),
+        var errorCalled = false;
+        var subscriber = testSubject.SubscribeAsync(a => throw new InvalidOperationException(),
             e => errorCalled = true);
-        
+
         testSubject.OnNext("hello");
         errorCalled.Should().BeTrue();
-    }    
-    
+    }
+
+    [Fact]
+    public void TestSubscribeAsyncExceptionShouldNotStopAfterException()
+    {
+        var testSubject = new Subject<string>();
+        var nrCalled = 0;
+        var subscriber = testSubject.SubscribeAsync(a => throw new InvalidOperationException(),
+            e => nrCalled++);
+
+        testSubject.OnNext("hello");
+        testSubject.OnNext("hello");
+
+        nrCalled.Should().Be(2);
+    }
+
+    [Fact]
+    public void TestSubscribeSafeExceptionShouldCallErrorCallback()
+    {
+        var testSubject = new Subject<string>();
+        var errorCalled = false;
+        var subscriber = testSubject.SubscribeSafe(a => throw new InvalidOperationException(),
+            e => errorCalled = true);
+
+        testSubject.OnNext("hello");
+        errorCalled.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TestSubscribeSafeMultipleExceptionShouldNotStopAfterException()
+    {
+        var testSubject = new Subject<string>();
+        var nrCalled = 0;
+        var subscriber = testSubject.SubscribeSafe(a => throw new InvalidOperationException(),
+            e => nrCalled++);
+
+        testSubject.OnNext("hello");
+        testSubject.OnNext("hello");
+
+        nrCalled.Should().Be(2);
+    }
+
     [Fact]
     public void TestSubscribeAsyncConcurrentExceptionShouldCallErrorCallback()
     {
         var testSubject = new Subject<string>();
-        bool errorCalled = false;
-        var subscriber = testSubject.SubscribeAsyncConcurrent((a) => throw new InvalidOperationException(),
+        var errorCalled = false;
+        var subscriber = testSubject.SubscribeAsyncConcurrent(a => throw new InvalidOperationException(),
             e => errorCalled = true);
-        
+
         testSubject.OnNext("hello");
         errorCalled.Should().BeTrue();
     }
