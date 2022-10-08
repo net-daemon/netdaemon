013ff8814d0d00f1c64356e507fbb7af5a6e3e53
Phillip Sawyer <30532122+phillipsawyer@users.noreply.github.com>
2022-10-07 20:44:30 -0600

    Initial commit, more attributes to come

--- src/HassModel/NetDeamon.HassModel/Entities/Core/LightAttributesBase.cs
+++ src/HassModel/NetDeamon.HassModel/Entities/Core/LightAttributesBase.cs
@@ -3,42 +3,90 @@
 
 public record LightAttributesBase
 {
-    [JsonPropertyName("entity_id")] public object? EntityId { get; init; }
+    [JsonPropertyName("entity_id")]
+    public object? EntityId { get; init; }
 
     /// <summary>
     /// Integer between 0 and 255 for how bright the light should be, where 0 means the light is off, 1 is the minimum brightness and 255 is the maximum brightness supported by the light.
     /// </summary>
-    [JsonPropertyName("brightness")] public int? Brightness { get; init; }
-
-    [JsonPropertyName("color_mode")] public string? ColorMode { get; init; }
-
-    [JsonPropertyName("effect_list")] public IReadOnlyList<string>? EffectList { get; init; }
-
-    [JsonPropertyName("friendly_name")] public string? FriendlyName { get; init; }
-
-    [JsonPropertyName("icon")] public string? Icon { get; init; }
+    [JsonPropertyName("brightness")]
+    public int? Brightness { get; init; }
     
-    [JsonPropertyName("max_mireds")] public double? MaxMireds { get; init; }
+    [JsonPropertyName("color_mode")]
+    public string? ColorMode { get; init; }
 
-    [JsonPropertyName("min_mireds")] public double? MinMireds { get; init; }
+    /// <summary>
+    /// The current effect.
+    /// </summary>
+    [JsonPropertyName("effect")]
+    public string? Effect { get; init; }
+    
+    /// <summary>
+    /// The list of supported effects.
+    /// </summary>
+    [JsonPropertyName("effect_list")]
+    public IReadOnlyList<string>? EffectList { get; init; }
 
-    [JsonPropertyName("off_brightness")] public object? OffBrightness { get; init; }
+    [JsonPropertyName("friendly_name")]
+    public string? FriendlyName { get; init; }
 
+    [JsonPropertyName("icon")]
+    public string? Icon { get; init; }
+    
+    /// <summary>
+    /// The warmest color_temp that this light supports
+    /// </summary>
+    [JsonPropertyName("max_mireds")]
+    public int? MaxMireds { get; init; }
+
+    /// <summary>
+    /// The coldest color_temp that this light supports.
+    /// </summary>
+    [JsonPropertyName("min_mireds")]
+    public int? MinMireds { get; init; }
+
+    [JsonPropertyName("off_brightness")]
+    public object? OffBrightness { get; init; }
+    
     [JsonPropertyName("supported_color_modes")]
     public IReadOnlyList<string>? SupportedColorModes { get; init; }
-
-    [JsonPropertyName("supported_features")]
-    public double? SupportedFeatures { get; init; }
     
+    [JsonPropertyName("supported_features")]
+    public int? SupportedFeatures { get; init; }
+    
+    /// <summary>
+    /// Integer in mireds representing the color temperature of the light.
+    /// </summary>
     [JsonPropertyName("color_temp")]
-    public IReadOnlyList<double>? ColorTemp { get; init; }
+    public int? ColorTemp { get; init; }
 
+    /// <summary>
+    /// List containing two floats representing the hue and saturation of the color of the light. Hue is scaled 0-360, and saturation is scaled 0-100.
+    /// </summary>
     [JsonPropertyName("hs_color")]
     public IReadOnlyList<double>? HsColor { get; init; }
 
+    /// <summary>
+    /// List containing three integers between 0 and 255 representing the RGB color (red, green, blue) of the light. Three comma-separated integers that represent the color in RGB.
+    /// </summary>
     [JsonPropertyName("rgb_color")]
-    public IReadOnlyList<double>? RgbColor { get; init; }
+    public IReadOnlyList<int>? RgbColor { get; init; }
+    
+    /// <summary>
+    /// List containing four integers between 0 and 255 representing the RGBW color (red, green, blue, white) of the light. This attribute will be null for lights which do not support RGBW colors.
+    /// </summary>
+    [JsonPropertyName("rgbw_color")]
+    public IReadOnlyList<int>? RgbwColor { get; init; }
+    
+    /// <summary>
+    /// List containing five integers between 0 and 255 representing the RGBWW color (red, green, blue, cold white, warm white) of the light. This attribute will be null for lights which do not support RGBWW colors.
+    /// </summary>
+    [JsonPropertyName("rgbww_color")]
+    public IReadOnlyList<int>? RgbwwColor { get; init; }
 
+    /// <summary>
+    /// List containing two floats representing the xy color of the light. Two comma-separated floats that represent the color in XY.
+    /// </summary>
     [JsonPropertyName("xy_color")]
     public IReadOnlyList<double>? XyColor { get; init; }
 }
---
+++ src/HassModel/NetDeamon.HassModel/Entities/Core/MediaPlayerAttributesBase.cs
@@ -0,0 +1,77 @@
+﻿namespace NetDaemon.HassModel.Entities.Core;
+#pragma warning disable CS1591
+
+public record MediaPlayerAttributesBase
+{
+    [JsonPropertyName("app_id")]
+    public string? AppId { get; init; }
+
+    [JsonPropertyName("app_name")]
+    public string? AppName { get; init; }
+
+    [JsonPropertyName("device_class")]
+    public string? DeviceClass { get; init; }
+
+    [JsonPropertyName("entity_id")]
+    public object? EntityId { get; init; }
+
+    [JsonPropertyName("entity_picture")]
+    public string? EntityPicture { get; init; }
+
+    [JsonPropertyName("entity_picture_local")]
+    public string? EntityPictureLocal { get; init; }
+
+    [JsonPropertyName("friendly_name")]
+    public string? FriendlyName { get; init; }
+
+    [JsonPropertyName("icon")]
+    public string? Icon { get; init; }
+
+    [JsonPropertyName("is_volume_muted")]
+    public bool? IsVolumeMuted { get; init; }
+
+    [JsonPropertyName("media_album_name")]
+    public string? MediaAlbumName { get; init; }
+
+    [JsonPropertyName("media_artist")]
+    public string? MediaArtist { get; init; }
+
+    [JsonPropertyName("media_content_id")]
+    public string? MediaContentId { get; init; }
+
+    [JsonPropertyName("media_content_type")]
+    public string? MediaContentType { get; init; }
+
+    [JsonPropertyName("media_duration")]
+    public double? MediaDuration { get; init; }
+
+    [JsonPropertyName("media_position")]
+    public double? MediaPosition { get; init; }
+
+    [JsonPropertyName("media_position_updated_at")]
+    public string? MediaPositionUpdatedAt { get; init; }
+
+    [JsonPropertyName("media_title")]
+    public string? MediaTitle { get; init; }
+
+    [JsonPropertyName("media_track")]
+    public double? MediaTrack { get; init; }
+
+    [JsonPropertyName("repeat")]
+    public string? Repeat { get; init; }
+
+    [JsonPropertyName("shuffle")]
+    public bool? Shuffle { get; init; }
+
+    [JsonPropertyName("source")]
+    public string? Source { get; init; }
+
+    [JsonPropertyName("source_list")]
+    public IReadOnlyList<string>? SourceList { get; init; }
+
+    [JsonPropertyName("supported_features")]
+    public double? SupportedFeatures { get; init; }
+
+    [JsonPropertyName("volume_level")]
+    public double? VolumeLevel { get; init; }
+}
