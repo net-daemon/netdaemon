e80fb3a7a9338d62508bd15fe47702208b6a1b01
Rick <rick-van-dam@outlook.com>
2023-04-24 20:56:53 +0200

    Add WhenStateIsFor overload with a non nullable scheduler parameter and provide a obsolete WhenStateIsFor without s scheduler parameter for backwards compatibility.

--- src/HassModel/NetDeamon.HassModel/StateObservableExtensions.cs
+++ src/HassModel/NetDeamon.HassModel/StateObservableExtensions.cs
@@ -10,18 +10,40 @@ public static class StateObservableExtensions
     /// <summary>
     /// Waits for an EntityState to match a predicate for the specified time
     /// </summary>
+    [Obsolete("Use the overload with IScheduler instead")]
+    public static IObservable<StateChange> WhenStateIsFor(
+        this IObservable<StateChange> observable, 
+        Func<EntityState?, bool> predicate,
+        TimeSpan timeSpan)
+        => observable.WhenStateIsFor(predicate, timeSpan, Scheduler.Default);
+
+    /// <summary>
+    /// Waits for an EntityState to match a predicate for the specified time
+    /// </summary>
+    [Obsolete("Use the overload with IScheduler instead")]
+    public static IObservable<StateChange<TEntity, TEntityState>> WhenStateIsFor<TEntity, TEntityState>(
+        this IObservable<StateChange<TEntity, TEntityState>> observable, 
+        Func<TEntityState?, bool> predicate, 
+        TimeSpan timeSpan)
+        where TEntity : Entity
+        where TEntityState : EntityState 
+        => observable.WhenStateIsFor(predicate, timeSpan, Scheduler.Default);
+    
+    /// <summary>
+    /// Waits for an EntityState to match a predicate for the specified time
+    /// </summary>
     public static IObservable<StateChange> WhenStateIsFor(
         this IObservable<StateChange> observable, 
         Func<EntityState?, bool> predicate,
         TimeSpan timeSpan,
-        IScheduler? scheduler = null)
+        IScheduler scheduler)
     
         => observable
             // Only process changes that start or stop matching the predicate
             .Where(e => predicate(e.Old) != predicate(e.New))
                 
             // Both  will restart the timer
-            .Throttle(timeSpan, scheduler ?? Scheduler.Default)
+            .Throttle(timeSpan, scheduler)
             
             // But only when the new state matches the predicate we emit it
             .Where(e => predicate(e.New));
@@ -33,12 +55,12 @@ public static class StateObservableExtensions
         this IObservable<StateChange<TEntity, TEntityState>> observable, 
         Func<TEntityState?, bool> predicate, 
         TimeSpan timeSpan,
-        IScheduler? scheduler = null)
+        IScheduler scheduler)
         where TEntity : Entity
         where TEntityState : EntityState 
 
         => observable
             .Where(e => predicate(e.Old) != predicate(e.New))
-            .Throttle(timeSpan, scheduler ?? Scheduler.Default)
+            .Throttle(timeSpan, scheduler)
             .Where(e => predicate(e.New));
 }
