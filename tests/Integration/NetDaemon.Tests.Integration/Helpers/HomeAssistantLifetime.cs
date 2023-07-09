53b52efe79e3c7e110493a546731876872b71186
Rick <rick-van-dam@outlook.com>
2023-07-09 21:58:07 +0200

    write homeassistant log to output

--- tests/Integration/NetDaemon.Tests.Integration/Helpers/HomeAssistantLifetime.cs
+++ tests/Integration/NetDaemon.Tests.Integration/Helpers/HomeAssistantLifetime.cs
@@ -3,11 +3,15 @@ using System.IO;
 using System.Threading.Tasks;
 using NetDaemon.Tests.Integration.Helpers.HomeAssistantTestContainer;
 using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace NetDaemon.Tests.Integration.Helpers;
 
 public class HomeAssistantLifetime : IAsyncLifetime
 {
+    private readonly IMessageSink _sink;
+
     private readonly HomeAssistantContainer _homeassistant = new HomeAssistantContainerBuilder()
         .WithResourceMapping(new DirectoryInfo("./HA/config"), "/config")
         .WithVersion(Environment.GetEnvironmentVariable("HomeAssistantVersion") ?? HomeAssistantContainerBuilder.DefaultVersion)
@@ -16,6 +20,11 @@ public class HomeAssistantLifetime : IAsyncLifetime
     public string? AccessToken;
     public ushort Port => _homeassistant.Port;
 
+    public HomeAssistantLifetime(IMessageSink sink)
+    {
+        _sink = sink;
+    }
+    
     public async Task InitializeAsync()
     {
         await _homeassistant.StartAsync();
@@ -26,6 +35,8 @@ public class HomeAssistantLifetime : IAsyncLifetime
 
     public async Task DisposeAsync()
     {
+        var (stdout, stderr) = await _homeassistant.GetLogsAsync();
+        _sink.OnMessage(new DiagnosticMessage(stderr));
         await _homeassistant.StopAsync();
     }
 }
