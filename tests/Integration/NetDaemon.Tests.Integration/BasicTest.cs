517985ef264f2aca708f75c730b3a389e7a13798
Rick <rick-van-dam@outlook.com>
2023-07-09 14:31:26 +0200

    reenable tests and run them against beta as well

--- tests/Integration/NetDaemon.Tests.Integration/BasicTest.cs
+++ tests/Integration/NetDaemon.Tests.Integration/BasicTest.cs
@@ -34,27 +34,26 @@ public class BasicApp
 public class BasicTests : NetDaemonIntegrationBase
 {
     [Fact]
-    public Task BasicTestApp_ShouldChangeStateOfInputTextToTheStateOfInputSelect_WhenChange()
+    public async Task BasicTestApp_ShouldChangeStateOfInputTextToTheStateOfInputSelect_WhenChange()
     {
-        return Task.CompletedTask;
-        //var haContext = Services.GetRequiredService<IHaContext>();
-        //var optionToSet = GetDifferentOptionThanCurrentlySelected(haContext);
-        //
-        //var waitTask = haContext.StateChanges()
-        //    .Where(n => n.Entity.EntityId == "input_text.test_result")
-        //    .FirstAsync()
-        //    .ToTask();
-        //
-        //haContext.CallService(
-        //    "input_select",
-        //    "select_option",
-        //    ServiceTarget.FromEntities("input_select.who_cooks"),
-        //    new {option = optionToSet});
-        //
-        //var act = async () => await waitTask.ConfigureAwait(false);
-        //
-        //var result = (await act.Should().CompleteWithinAsync(5000.Milliseconds())).Subject;
-        //result.New!.State.Should().Be(optionToSet);
+        var haContext = Services.GetRequiredService<IHaContext>();
+        var optionToSet = GetDifferentOptionThanCurrentlySelected(haContext);
+        
+        var waitTask = haContext.StateChanges()
+            .Where(n => n.Entity.EntityId == "input_text.test_result")
+            .FirstAsync()
+            .ToTask();
+        
+        haContext.CallService(
+            "input_select",
+            "select_option",
+            ServiceTarget.FromEntities("input_select.who_cooks"),
+            new {option = optionToSet});
+        
+        var act = async () => await waitTask.ConfigureAwait(false);
+        
+        var result = (await act.Should().CompleteWithinAsync(5000.Milliseconds())).Subject;
+        result.New!.State.Should().Be(optionToSet);
     }
 
     private string GetDifferentOptionThanCurrentlySelected(IHaContext haContext)
--- tests/Integration/NetDaemon.Tests.Integration/CodegenIntegrationTests.cs
+++ tests/Integration/NetDaemon.Tests.Integration/CodegenIntegrationTests.cs
@@ -19,20 +19,19 @@ public class CodegenIntegrationTests : NetDaemonIntegrationBase
     ///     beta stage
     /// </summary>
     [Fact]
-    public Task Codegen_ShouldBeAbleToParseServiceDescriptions()
+    public async Task Codegen_ShouldBeAbleToParseServiceDescriptions()
     {
-        return Task.CompletedTask;
-        //var haConnection = Services.GetRequiredService<IHomeAssistantConnection>();
-        //
-        //var element = await haConnection.GetServicesAsync(new CancellationTokenSource(TimeSpan.FromSeconds(20)).Token).ConfigureAwait(false) ?? throw new InvalidOperationException("Failed to get services");
-        //var serviceMetadata = ServiceMetaDataParser.Parse(element);
-        //serviceMetadata.Count.Should().NotBe(0);
-        //
-        //var lightDomain = serviceMetadata.FirstOrDefault(n => n.Domain == "switch") ?? throw new InvalidOperationException("Expected domain light to be present");
-        //
-        //var turnOnService = lightDomain.Services.FirstOrDefault(n => n.Service == "turn_on") ?? throw new InvalidOperationException("Expected domain light to be present");
-        //
-        //Assert.NotNull(turnOnService?.Target?.Entity);
+        var haConnection = Services.GetRequiredService<IHomeAssistantConnection>();
+        
+        var element = await haConnection.GetServicesAsync(new CancellationTokenSource(TimeSpan.FromSeconds(20)).Token).ConfigureAwait(false) ?? throw new InvalidOperationException("Failed to get services");
+        var serviceMetadata = ServiceMetaDataParser.Parse(element);
+        serviceMetadata.Count.Should().NotBe(0);
+        
+        var lightDomain = serviceMetadata.FirstOrDefault(n => n.Domain == "switch") ?? throw new InvalidOperationException("Expected domain light to be present");
+        
+        var turnOnService = lightDomain.Services.FirstOrDefault(n => n.Service == "turn_on") ?? throw new InvalidOperationException("Expected domain light to be present");
+        
+        Assert.NotNull(turnOnService?.Target?.Entity);
     }
 
     public CodegenIntegrationTests(HomeAssistantLifetime homeAssistantLifetime) : base(homeAssistantLifetime)
