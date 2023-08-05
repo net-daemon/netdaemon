c77cb0f5613087e848b0e08ff9036c9fcb91b31c
Tomas Hellström <tomas.hellstrom@yahoo.se>
2023-08-05 12:08:39 +0200

    Add default metadata and deprecate UseAttributeBaseClasses setting

---
+++ src/HassModel/NetDaemon.HassModel.Tests/CodeGenerator/ControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using NetDaemon.Client.Settings;
+using NetDaemon.HassModel.CodeGenerator;
+
+namespace NetDaemon.HassModel.Tests.CodeGenerator;
+
+public class ControllerTest
+{
+    [Fact]
+    public async Task ControllerShouldReturnDefaultValueForMetadata()
+    {
+        // ARRANGE
+        var controller = new Controller(new CodeGenerationSettings(), new HomeAssistantSettings());
+
+        // ACT
+        var result = await controller.LoadEntitiesMetaDataAsync();
+
+        // ASSERT
+        result.Domains.Count.Should().Be(1);
+        result.Domains.Single(x => x.Domain == "light").Should().NotBeNull();
+    }
+}
