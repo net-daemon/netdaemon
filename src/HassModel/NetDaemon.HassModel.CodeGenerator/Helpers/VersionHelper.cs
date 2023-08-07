8097e19c3972e05b0939dd506edd04d995c1d99b
Tomas Hellström <tomas.hellstrom@yahoo.se>
2023-08-07 09:16:18 +0200

    Fixed comments

--- src/HassModel/NetDaemon.HassModel.CodeGenerator/Helpers/VersionHelper.cs
+++ src/HassModel/NetDaemon.HassModel.CodeGenerator/Helpers/VersionHelper.cs
@@ -5,13 +5,19 @@ namespace NetDaemon.HassModel.CodeGenerator.Helpers;
 #pragma warning disable CA1303
 
 /// <summary>
-/// Returns current version of NetDaemon
+/// Helper class for managing NetDaemon version tasks
 /// </summary>
 public static class VersionHelper
 {
+    /// <summary>
+    /// Returns current version of NetDaemon
+    /// </summary>
     public static string GeneratorVersion { get; } =
         Assembly.GetAssembly(typeof(Generator))!.GetName().Version!.ToString();
 
+    /// <summary>
+    /// Pretty prints version information to console
+    /// </summary>
     public static void PrintVersion()
     {
         Console.Write("Codegen version: ");
