554348a997014efd52be06668df12889043d3842
Tomas Hellström <tomas.hellstrom@yahoo.se>
2023-08-07 08:41:17 +0200

    Added version information on code generation

---
+++ src/HassModel/NetDaemon.HassModel.CodeGenerator/Helpers/VersionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace NetDaemon.HassModel.CodeGenerator.Helpers;
+
+/// <summary>
+/// Returns current version of NetDaemon
+/// </summary>
+public class VersionHelper
+{
+    public static string GeneratorVersion { get; } =
+        Assembly.GetAssembly(typeof(Generator))!.GetName().Version!.ToString();
+
+    public static void PrintVersion()
+    {
+        Console.Write("Codegen version: ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(GeneratorVersion);
+        Console.ResetColor();
+    }
+}
