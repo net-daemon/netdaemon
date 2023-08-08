6a074d0a130e4da45ceb1fbf72995cc42155945b
Scott Leckie <scott.leckie@kwolo.com>
2023-08-08 21:19:14 +0100

    Add some comments on how to test the MQTT entity subscriptions

--- src/debug/DebugHost/apps/Extensions/MttEntitySubscriptionApp.cs
+++ src/debug/DebugHost/apps/Extensions/MttEntitySubscriptionApp.cs
@@ -28,39 +28,48 @@ public class MttEntitySubscriptionApp : IAsyncInitializable
         await Task.Run(() => ExercisorAsync(cancellationToken), cancellationToken).ConfigureAwait(false);
     }
 
+    /// <summary>
+    /// Important: All this method does is set up the subscriptions that will log when the test switches are toggled
+    /// To verify this is running:
+    ///     1. Run the code
+    ///     2. Check your HomeAssistant instance and you should see two switches named "One" and "Two"
+    ///     3. Toggle them and ensure that the a message is logged (see code within PrepareCommandSubscriptionAsync...)
+    ///     4. (optionally) uncomment the code at the end to remove the test switches, rebuild and re-run
+    /// </summary>
+    /// <param name="cancellationToken"></param>
     private async Task ExercisorAsync(CancellationToken cancellationToken)
     {
-        var switch1Id = "switch.switch_one";
-        var switch2Id = "switch.switch_two";
-        var onCommand = "ON";
-        var offCommand = "OFF";
+        const string switch1Id = "switch.switch_one";
+        const string switch2Id = "switch.switch_two";
+        const string onCommand = "ON";
+        const string offCommand = "OFF";
 
         await _entityManager.CreateAsync(switch1Id,
                 new EntityCreationOptions(Name: "Switch One", PayloadOn: onCommand, PayloadOff: offCommand))
             .ConfigureAwait(false);
-        
+
         await _entityManager.CreateAsync(switch2Id,
                 new EntityCreationOptions(Name: "Switch Two", PayloadOn: onCommand, PayloadOff: offCommand))
             .ConfigureAwait(false);
-        
+
         (await _entityManager.PrepareCommandSubscriptionAsync(switch1Id).ConfigureAwait(false)).Subscribe(new Action<string>(async s =>
         {
             _logger.LogInformation("Subscription #1a got command for {Switch} {Cmd}", switch1Id, s);
             await Task.Yield();     // Achieves nothing, just masks the CS1998 warning
         }));
-        
+
         (await _entityManager.PrepareCommandSubscriptionAsync(switch1Id).ConfigureAwait(false)).Subscribe(new Action<string>(async s =>
         {
             _logger.LogInformation("Subscription #1b got command for {Switch} {Cmd}", switch1Id, s);
             await Task.Yield();     // Achieves nothing, just masks the CS1998 warning
         }));
-        
+
         (await _entityManager.PrepareCommandSubscriptionAsync(switch2Id).ConfigureAwait(false)).Subscribe(new Action<string>(async s =>
         {
             _logger.LogInformation("Subscription #2 got command for {Switch} {Cmd}", switch2Id, s);
             await Task.Yield();     // Achieves nothing, just masks the CS1998 warning
         }));
-        
+
         // Thread.Sleep(2000);
        // await _entityManager.RemoveAsync(switch1Id).ConfigureAwait(false);
        // await _entityManager.RemoveAsync(switch2Id).ConfigureAwait(false);
