feffd8cf1418da81267bc9d519ffb900a3f0e7d4
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2023-10-24 07:04:08 +0200

    Move dynamic compile to NetDaemon.AppModel.SourceDeployedApps.csproj (#959)

--- src/AppModel/NetDaemon.AppModel.SourceDeployedApps/Compiler/SyntaxTreeResolver.cs
+++ src/AppModel/NetDaemon.AppModel.SourceDeployedApps/Compiler/SyntaxTreeResolver.cs
@@ -1,6 +1,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.Extensions.Options;
 
 namespace NetDaemon.AppModel.Internal.Compiler;
 
