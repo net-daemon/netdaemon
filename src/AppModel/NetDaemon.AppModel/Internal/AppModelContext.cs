3e86cf6e59f1e05e607e6918997cf2d16e940506
helto4real <tomas.hellstrom@yahoo.se>
2023-10-26 06:09:28 +0200

    handle unloading of faulty apps better and prohibit apps instancing if previous unloading fails

--- src/AppModel/NetDaemon.AppModel/Internal/AppModelContext.cs
+++ src/AppModel/NetDaemon.AppModel/Internal/AppModelContext.cs
@@ -26,7 +26,7 @@ internal class AppModelContext : IAppModelContext
     {
         var factories = _appFactoryProviders.SelectMany(provider => provider.GetAppFactories()).ToList();
 
-        var filteredFactories =  _focusFilter.FilterFocusApps(factories);
+        var filteredFactories = _focusFilter.FilterFocusApps(factories);
 
         foreach (var factory in filteredFactories)
         {
@@ -44,11 +44,33 @@ internal class AppModelContext : IAppModelContext
         if (_isDisposed) return;
         _isDisposed = true;
 
-        foreach (var appInstance in _applications)
-        {
-            await appInstance.DisposeAsync().ConfigureAwait(false);
-        }
+        // Get all tasks for disposing the apps with a timeout
+        var disposeTasks = _applications.Select(app => app.DisposeAsync().AsTask().WaitAsync(TimeSpan.FromSeconds(3))).ToList();
 
+        // We cannot use Task WhenAll here since we do not want to halt on a single exception
+        // but rather log all exceptions and make sure all apps are disposed properly
+        foreach (var disposeTask in disposeTasks)
+        {
+            try
+            {
+               await disposeTask.ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                var indexOfApp = disposeTasks.IndexOf(disposeTask);
+                var app = _applications[indexOfApp];
+
+                if (e is TimeoutException)
+                    _logger.LogError("Unloading the app '{App}' takes longer than expected, please check for blocking code", app.Id);
+                else
+                    _logger.LogError(disposeTask.Exception, "Unloading the app '{App}' failed", app.Id);
+            }
+        }
+        _logger.LogInformation("All apps are disposed");
         _applications.Clear();
+
+        // We need to force a garbage collection here to make sure all apps are freed from memory
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
     }
 }
