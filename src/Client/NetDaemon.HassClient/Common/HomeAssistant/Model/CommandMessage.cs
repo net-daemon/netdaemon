1856886436f445326f3d45b5dac45dfd90769e07
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2023-11-16 15:45:45 +0100

    Log correct json content of messages sent to HA on failed commands  (#1001)
    * Log full command json when sending command fails
    
    * Fix flaky tests

--- src/Client/NetDaemon.HassClient/Common/HomeAssistant/Model/CommandMessage.cs
+++ src/Client/NetDaemon.HassClient/Common/HomeAssistant/Model/CommandMessage.cs
@@ -2,5 +2,9 @@
 
 public record CommandMessage : HassMessageBase
 {
+    private static readonly JsonSerializerOptions NonIndentingJsonSerializerOptions = new() { WriteIndented = false };
+
     [JsonPropertyName("id")] public int Id { get; set; }
+
+    public string GetJsonString() => JsonSerializer.Serialize(this, GetType(), NonIndentingJsonSerializerOptions);
 }
