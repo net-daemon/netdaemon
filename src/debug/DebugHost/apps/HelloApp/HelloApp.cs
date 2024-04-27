45b0c11f1c48489096345c9c085bfed7dab5ec16
Frank Bakker <bakker.frank@gmail.com>
2024-04-27 21:32:07 +0200

    Clean debug code

--- src/debug/DebugHost/apps/HelloApp/HelloApp.cs
+++ src/debug/DebugHost/apps/HelloApp/HelloApp.cs
@@ -2,27 +2,23 @@ using System.Reactive.Linq;
 using Microsoft.Extensions.Logging;
 using NetDaemon.AppModel;
 using NetDaemon.HassModel;
-using NetDaemon.HassModel.Entities;
 
 namespace Apps;
 
 [NetDaemonApp]
 [Focus]
-public sealed class HelloApp2 : IAsyncDisposable
+public sealed class HelloApp : IAsyncDisposable
 {
-    private readonly ILogger<HelloApp2> _logger;
+    private readonly ILogger<HelloApp> _logger;
 
-    public HelloApp2(IHaContext ha, ILogger<HelloApp2> logger, IHaRegistry registry)
+    public HelloApp(IHaContext ha, ILogger<HelloApp> logger)
     {
-        var home = new Home(registry);
-        var x = home.Floors.Downstairs.Areas.SelectMany(a => a.Entities);
-
         _logger = logger;
-        var boilerConnected = ha.Entity("switch.heating_valve_kitchen");
-        var labels = boilerConnected.Registration?.Labels;
-        var all = registry.Entities;
-        var criticalEntities = ha.GetAllEntities().Where(e => e.Registration?.Labels.Any(l => l.Name == "critical") ?? false);
-        //criticalEntities.StateChanges().Where(s => s.New?.State == "unavailable").Subscribe(e => logger.LogCritical("Entity {Entity} became unavailable", e.Entity.EntityId));
+        ha.Events.Where(n => n.EventType == "test_event").Subscribe( _ =>
+        {
+            logger.LogInformation("Hello testevent");
+        });
+        ha.CallService("notify", "persistent_notification", data: new { message = "Notify me", title = "Hello world!" });
     }
 
     public async ValueTask DisposeAsync()
@@ -31,23 +27,4 @@ public sealed class HelloApp2 : IAsyncDisposable
         _logger.LogInformation("disposed app");
         //return ValueTask.CompletedTask;
     }
-}
-
-record Home(IHaRegistry HaRegistry)
-{
-    public MyFloors Floors => new MyFloors(HaRegistry);
-}
-
-record MyFloors(IHaRegistry Registry)
-{
-    public Floor Downstairs => Registry.GetFloor("downstairs")!;
-    public Floor Upstairs => Registry.GetFloor("upstairs")!;
-    public Floor Attic => Registry.GetFloor("upstairs")!;
-}
-
-record AtticFloorAreas(IHaRegistry HaRegistry)
-{
-    public IEnumerable<Area>? Areas { get; }
-    public Area Study => HaRegistry.GetArea("study")!;
-
 }
