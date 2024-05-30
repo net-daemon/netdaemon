7a790f3c14e8297192640944510143d8bfc4cd3c
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-05-30 21:50:18 +0200

    removed the enumerable extension that is not needed

--- src/HassModel/NetDeamon.HassModel/Entities/EnumerableEntityExtensions.cs
+++ src/HassModel/NetDeamon.HassModel/Entities/EnumerableEntityExtensions.cs
@@ -102,40 +102,4 @@ public static class EnumerableEntityExtensions
             group.Key.CallService(serviceDomain, serviceName, new ServiceTarget { EntityIds = group.Select(e => e.EntityId).ToList() }, data);
         }
     }
-    /// <summary>
-    /// Calls a service with a set of Entities as the target
-    /// </summary>
-    /// <param name="entities">IEnumerable of Entities for which to call the service</param>
-    /// <param name="service">Name of the service to call. If the Domain of the service is the same as the domain of the Entities it can be omitted</param>
-    /// <param name="data">Data to provide</param>
-    public static async Task<IEnumerable<JsonElement?>> CallServiceWithResponseAsync(this IEnumerable<IEntityCore> entities, string service, object? data = null)
-    {
-        ArgumentNullException.ThrowIfNull(service);
-
-        entities = entities.ToList();
-
-        if (!entities.Any()) return [];
-
-        var (serviceDomain, serviceName) = service.SplitAtDot();
-
-        if (serviceDomain == null)
-        {
-            var domainsFromEntity = entities.Select(e => e.EntityId.SplitAtDot().Left).Distinct().Take(2).ToArray();
-            if (domainsFromEntity.Length != 1) throw new InvalidOperationException($"Cannot call service {service} for entities that do not have the same domain");
-
-            serviceDomain = domainsFromEntity.First()!;
-        }
-
-        // Usually each Entity will have the same IHaContext and domain, but just in case its not, group by the context and domain and call the
-        // service for each group separately
-        var serviceCalls = entities.GroupBy(e => e.HaContext);
-        var returnValues = new List<JsonElement?>();
-
-        foreach (var group in serviceCalls)
-        {
-            returnValues.Add(await group.Key.CallServiceWithResponseAsync(serviceDomain, serviceName, new ServiceTarget { EntityIds = group.Select(e => e.EntityId).ToList() }, data));
-        }
-        return returnValues;
-
-    }
 }
