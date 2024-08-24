e312b54e9a5f9f5c967fcea0358b654b6b7e8e66
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2024-08-24 23:46:20 +0200

    Fix: Registry update fails when Device Model property is an array (#1160)
    Co-authored-by: Tomas Hellström <tomas.hellstrom@yahoo.se>

--- src/Client/NetDaemon.HassClient.Tests/Json/EnsureStringConverterTests.cs
+++ src/Client/NetDaemon.HassClient.Tests/Json/EnsureStringConverterTests.cs
@@ -3,35 +3,26 @@ namespace NetDaemon.HassClient.Tests.Json;
 
 public class EnsureStringConverterTests
 {
-    /// <summary>
-    ///     Default Json serialization options, Hass expects intended
-    /// </summary>
-    private readonly JsonSerializerOptions _defaultSerializerOptions = new()
-    {
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
-
     [Fact]
     public void TestConvertAValidString()
     {
-        const string jsonDevice = @"
-        {
-            ""config_entries"": [],
-            ""connections"": [],
-            ""manufacturer"": ""Google Inc."",
-            ""model"": 123123,
-            ""name"": 123,
-            ""serial_number"": ""1.0"",
-            ""sw_version"": ""100"",
-            ""hw_version"": null,
-            ""id"": ""42cdda32a2a3428e86c2e27699d79ead"",
-            ""via_device_id"": null,
-            ""area_id"": null,
-            ""name_by_user"": null
-        }
-        ";
-        var hassDevice = JsonSerializer.Deserialize<HassDevice>(jsonDevice, _defaultSerializerOptions);
+        var hassDevice = JsonSerializer.Deserialize<HassDevice>("""
+                {
+                    "config_entries": [],
+                    "connections": [],
+                    "manufacturer": "Google Inc.",
+                    "model": 123123,
+                    "name": 123,
+                    "serial_number": "1.0",
+                    "sw_version": "100",
+                    "hw_version": null,
+                    "id": "42cdda32a2a3428e86c2e27699d79ead",
+                    "via_device_id": null,
+                    "area_id": null,
+                    "name_by_user": null
+                }
+        """);
+
         hassDevice!.SerialNumber.Should().Be("1.0");
         hassDevice!.SoftwareVersion.Should().Be("100");
         hassDevice!.HardwareVersion.Should().BeNull();
@@ -40,30 +31,53 @@ public class EnsureStringConverterTests
     [Fact]
     public void TestConverAInvalidString()
     {
-        const string jsonDevice = @"
-        {
-            ""config_entries"": [],
-            ""connections"": [],
-            ""model"": ""Chromecast"",
-            ""serial_number"": [""1.0"", ""2.0""],
-            ""name"": ""My TV"",
-            ""sw_version"": { ""attribute"": ""1.0"" },
-            ""manufacturer"": ""Google Inc."",
-            ""hw_version"": 100,
-            ""id"": ""42cdda32a2a3428e86c2e27699d79ead"",
-            ""via_device_id"": null,
-            ""area_id"": null,
-            ""name_by_user"": null
-        }
-        ";
-        var hassDevice = JsonSerializer.Deserialize<HassDevice>(jsonDevice, _defaultSerializerOptions);
+        var hassDevice = JsonSerializer.Deserialize<HassDevice>("""
+            {
+                "config_entries": [],
+                "connections": [],
+                "model": "Chromecast",
+                "serial_number": ["1.0", "2.0"],
+                "name": "My TV",
+                "sw_version": { "attribute": "1.0" },
+                "manufacturer": "Google Inc.",
+                "hw_version": 100,
+                "id": "42cdda32a2a3428e86c2e27699d79ead",
+                "via_device_id": null,
+                "area_id": null,
+                "name_by_user": null
+            }
+        """);
+
         hassDevice!.SerialNumber.Should().BeNull();
         hassDevice!.SoftwareVersion.Should().BeNull();
-        hassDevice!.HardwareVersion.Should().BeNull();
+        hassDevice!.HardwareVersion.Should().Be("100");
 
         // Make sure it is skipped correctly by checking the next property is read
         hassDevice!.Manufacturer.Should().Be("Google Inc.");
         hassDevice!.Id.Should().Be("42cdda32a2a3428e86c2e27699d79ead");
         hassDevice!.Name.Should().Be("My TV");
+    }
+
+    [Fact]
+    public void TestConverModelIsArrayInvalidString()
+    {
+        var hassDevice = JsonSerializer.Deserialize<HassDevice>("""
+            {
+                "config_entries": [],
+                "connections": [],
+                "model": ["Chromecast"],
+                "serial_number": ["1.0", "2.0"],
+                "name": "My TV",
+                "sw_version": { "attribute": "1.0" },
+                "manufacturer": "Google Inc.",
+                "hw_version": 100,
+                "id": "42cdda32a2a3428e86c2e27699d79ead",
+                "via_device_id": null,
+                "area_id": null,
+                "name_by_user": null
+            }
+        """);
+
+        hassDevice!.Model.Should().BeNull();
     }
 }
--- src/Client/NetDaemon.HassClient/Common/HomeAssistant/Model/HassDevice.cs
+++ src/Client/NetDaemon.HassClient/Common/HomeAssistant/Model/HassDevice.cs
@@ -5,14 +5,13 @@ public record HassDevice
     [JsonPropertyName("manufacturer")] public string? Manufacturer { get; init; }
 
     [JsonPropertyName("model")]
-    [JsonConverter(typeof(ReadNumberAsStringConverter))]
+    [JsonConverter(typeof(EnsureStringConverter))]
     public string? Model { get; init; }
 
     [JsonPropertyName("id")] public string? Id { get; init; }
 
     [JsonPropertyName("area_id")] public string? AreaId { get; init; }
-
-    [JsonConverter(typeof(ReadNumberAsStringConverter))]
+    [JsonConverter(typeof(EnsureStringConverter))]
     [JsonPropertyName("name")] public string? Name { get; init; }
 
     [JsonPropertyName("name_by_user")] public string? NameByUser { get; init; }
