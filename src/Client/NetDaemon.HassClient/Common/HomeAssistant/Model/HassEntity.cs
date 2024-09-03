abb2305fa89473679c41df512bed2d91d5ca77e7
Steven Pelech <steven.pelech@gmail.com>
2024-09-03 07:44:20 -0500

    add deserialization off entity conversation options

--- src/Client/NetDaemon.HassClient/Common/HomeAssistant/Model/HassEntity.cs
+++ src/Client/NetDaemon.HassClient/Common/HomeAssistant/Model/HassEntity.cs
@@ -15,4 +15,7 @@ public record HassEntity
     [JsonPropertyName("platform")] public string? Platform { get; init; }
 
     [JsonPropertyName("labels")] public IReadOnlyList<string> Labels { get; init; } = [];
+	        
+    [JsonPropertyName("options")]public HassEntityOptions? Options { get; init; }
 }
+
