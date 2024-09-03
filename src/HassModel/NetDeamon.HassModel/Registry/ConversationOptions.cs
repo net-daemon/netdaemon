bc0e4260358f4207a6e95506774b50ff27161376
Steven Pelech <steven.pelech@gmail.com>
2024-09-03 07:44:33 -0500

    add conversation options to HassModel

---
+++ src/HassModel/NetDeamon.HassModel/Registry/ConversationOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace NetDaemon.HassModel.Entities
+{
+    /// <summary>
+    /// The options related to Assist/Conversation Assistants
+    /// </summary>
+    public record ConversationOptions
+    {
+        /// <summary>
+        /// If the entity should be available to voice/ai assistants
+        /// </summary>
+        public bool? ShouldExpose { get; init; }
+    }
+}
---
+++ src/HassModel/NetDeamon.HassModel/Registry/EntityOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace NetDaemon.HassModel.Entities;
+
+/// <summary>
+/// Details of Options in the Home Assistant Registry
+/// </summary>
+public record EntityOptions
+{
+    /// <summary>
+    /// The Assist/Conversation options for a registration entity
+    /// </summary>
+    public ConversationOptions? ConversationOptions { get; init; }
+}
+
