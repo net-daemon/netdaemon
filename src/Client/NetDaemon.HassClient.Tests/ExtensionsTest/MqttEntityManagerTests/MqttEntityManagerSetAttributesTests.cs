11d7979167ceff0191f6aabd06369d59741246b9
Scott Leckie <scott.leckie@kwolo.com>
2025-03-03 23:51:04 +0000

    Final two tests for MqttEntityManager message sending: SetAttributes and SetAvailability

---
+++ src/Client/NetDaemon.HassClient.Tests/ExtensionsTest/MqttEntityManagerTests/MqttEntityManagerSetAttributesTests.cs
@@ -0,0 +1,72 @@
+﻿using NetDaemon.Extensions.MqttEntityManager;
+using NetDaemon.HassClient.Tests.ExtensionsTest.MqttEntityManagerTests.TestHelpers;
+
+namespace NetDaemon.HassClient.Tests.ExtensionsTest.MqttEntityManagerTests;
+
+/// <summary>
+/// Note that there are so many tests relating to <see cref="MqttEntityManager"/> that they are broken into
+/// multiple test classes, all MqttEntityManager[OPERATION]Tests
+/// </summary>
+public class MqttEntityManagerSetAttributesTests
+{
+    [Fact]
+    public async Task SetAttributesSetsCorrectTopic()
+    {
+        var mocks = new MockMqttEntityManagerHelper();
+
+        var entityManager = new MqttEntityManager(mocks.MessageSender, mocks.MessageSubscriber, mocks.Options);
+
+        await entityManager.SetAttributesAsync("sensor.test", new { a1 = "one", a2 = 2 });
+
+        mocks.CapturedTopic.Should().Be("HomeAssistant/sensor/test/attributes");
+    }
+
+    [Fact]
+    public async Task SetAttributesCanSetSimplePayload()
+    {
+        var mocks = new MockMqttEntityManagerHelper();
+
+        var entityManager = new MqttEntityManager(mocks.MessageSender, mocks.MessageSubscriber, mocks.Options);
+
+        await entityManager.SetAttributesAsync("sensor.test", new { a1 = "one", a2 = 2 });
+
+        var parsedPayload = JsonDocument.Parse(mocks.CapturedPayload!);
+        parsedPayload.RootElement.GetProperty("a1").GetString().Should().Be("one");
+        parsedPayload.RootElement.GetProperty("a2").GetInt32().Should().Be(2);
+    }
+
+    [Fact]
+    public async Task SetAttributesCanSetComplexPayload()
+    {
+        var mocks = new MockMqttEntityManagerHelper();
+
+        var entityManager = new MqttEntityManager(mocks.MessageSender, mocks.MessageSubscriber, mocks.Options);
+
+        var complexObject = new
+        {
+            a1 = "one", a2 = 2, a3 = new
+            {
+                a31 = "three", a32 = 3
+            }
+        };
+        await entityManager.SetAttributesAsync("sensor.test", complexObject);
+
+        var parsedPayload = JsonDocument.Parse(mocks.CapturedPayload!);
+        parsedPayload.RootElement.GetProperty("a1").GetString().Should().Be("one");
+        parsedPayload.RootElement.GetProperty("a2").GetInt32().Should().Be(2);
+        parsedPayload.RootElement.GetProperty("a3").GetProperty("a31").GetString().Should().Be("three");
+        parsedPayload.RootElement.GetProperty("a3").GetProperty("a32").GetInt32().Should().Be(3);
+    }
+
+    [Fact]
+    public async Task SetAttributesSetsRetainFlag()
+    {
+        var mocks = new MockMqttEntityManagerHelper();
+
+        var entityManager = new MqttEntityManager(mocks.MessageSender, mocks.MessageSubscriber, mocks.Options);
+
+        await entityManager.SetAttributesAsync("sensor.test", new { a1 = "one", a2 = 2 });
+
+        mocks.CapturedRetain.Should().BeTrue();
+    }
+}
---
+++ src/Client/NetDaemon.HassClient.Tests/ExtensionsTest/MqttEntityManagerTests/MqttEntityManagerSetAvailabilityTests.cs
@@ -0,0 +1,47 @@
+﻿using NetDaemon.Extensions.MqttEntityManager;
+using NetDaemon.HassClient.Tests.ExtensionsTest.MqttEntityManagerTests.TestHelpers;
+
+namespace NetDaemon.HassClient.Tests.ExtensionsTest.MqttEntityManagerTests;
+
+/// <summary>
+/// Note that there are so many tests relating to <see cref="MqttEntityManager"/> that they are broken into
+/// multiple test classes, all MqttEntityManager[OPERATION]Tests
+/// </summary>
+public class MqttEntityManagerSetAvailabilityTests
+{
+    [Fact]
+    public async Task SetAvailabilitySetsCorrectTopic()
+    {
+        var mocks = new MockMqttEntityManagerHelper();
+
+        var entityManager = new MqttEntityManager(mocks.MessageSender, mocks.MessageSubscriber, mocks.Options);
+
+        await entityManager.SetAvailabilityAsync("sensor.test", "down");
+
+        mocks.CapturedTopic.Should().Be("HomeAssistant/sensor/test/availability");
+    }
+
+    [Fact]
+    public async Task SetAvailabilitySetsCorrectPayload()
+    {
+        var mocks = new MockMqttEntityManagerHelper();
+
+        var entityManager = new MqttEntityManager(mocks.MessageSender, mocks.MessageSubscriber, mocks.Options);
+
+        await entityManager.SetAvailabilityAsync("sensor.test", "down");
+
+        mocks.CapturedPayload.Should().Be("down");
+    }
+
+    [Fact]
+    public async Task SetAvailabilitySetsRetainFlag()
+    {
+        var mocks = new MockMqttEntityManagerHelper();
+
+        var entityManager = new MqttEntityManager(mocks.MessageSender, mocks.MessageSubscriber, mocks.Options);
+
+        await entityManager.SetAvailabilityAsync("sensor.test", "down");
+
+        mocks.CapturedRetain.Should().BeTrue();
+    }
+}
