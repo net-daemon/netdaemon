cfd1a3284e013092a00bb13acd6f4431367be8da
Jhonattan Souza <19364782+Jhonattan-Souza@users.noreply.github.com>
2025-03-16 02:42:14 -0300

    Refactor service registration methods in IntegrationHaContextExtensions (#1283)

--- src/HassModel/NetDaemon.HassModel.Integration/IntegrationHaContextExtensions.cs
+++ src/HassModel/NetDaemon.HassModel.Integration/IntegrationHaContextExtensions.cs
@@ -14,18 +14,30 @@ public static class IntegrationHaContextExtensions
     /// <param name="serviceName">The name of the service to create in Home Assistant. Will be prefixes with 'NetDaemon.'</param>
     /// <param name="callBack">The Action to invoke when the Service is called</param>
     /// <typeparam name="T">Type to deserialize the payload of the service into</typeparam>
-    public static void RegisterServiceCallBack<T>(this IHaContext haContext, string serviceName, Action<T> callBack)
+    public static void RegisterServiceCallBack<T>(this IHaContext haContext, string serviceName, Action<T> callBack) =>
+        RegisterService<T>(haContext, serviceName).Subscribe(callBack);
+
+    /// <summary>
+    /// Creates a service in Home Assistant and returns an IObservable to use methods on
+    /// </summary>
+    /// <param name="haContext">IHaContext to use</param>
+    /// <param name="serviceName">The name of the service to create in Home Assistant. Will be prefixes with 'NetDaemon.'</param>
+    /// <typeparam name="T">Type to deserialize the payload of the service into</typeparam>
+    /// <returns>IObservable to use methods on</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static IObservable<T> RegisterService<T>(this IHaContext haContext, string serviceName)
     {
         ArgumentNullException.ThrowIfNull(haContext);
+
         if (string.IsNullOrWhiteSpace(serviceName))
             throw new ArgumentException("serviceName must have a value", serviceName);
 
         haContext.CallService("netdaemon", "register_service", data: new { service = serviceName });
 
-        haContext.Events.Filter<HassServiceEventData<T>>("call_service")
+        return haContext.Events.Filter<HassServiceEventData<T>>("call_service")
             .Where(e => e.Data?.domain == "netdaemon"
                         && string.Equals(e.Data?.service, serviceName, StringComparison.OrdinalIgnoreCase))
-            .Subscribe(e => callBack.Invoke(e.Data!.service_data));
+            .Select(x => x.Data!.service_data);
     }
 
     /// <summary>
