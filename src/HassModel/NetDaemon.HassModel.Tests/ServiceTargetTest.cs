6bcfe26ea7d7e31e6c09cad4b1131d08a83a22cc
Ed Lenoir <edleno@comcast.net>
2025-07-31 20:12:42 -0700

    Added non-equal test cases to ServiceTargetTest

--- src/HassModel/NetDaemon.HassModel.Tests/ServiceTargetTest.cs
+++ src/HassModel/NetDaemon.HassModel.Tests/ServiceTargetTest.cs
@@ -37,11 +37,27 @@ public class ServiceTargetTest
     }
 
     [Fact]
+    public void ServiceTargetAndObjectShouldNotBeEqual()
+    {
+        var serviceTarget1 = ServiceTarget.FromEntity("light.kitchen");
+        object serviceTarget2 = ServiceTarget.FromEntity("light.livingroom");
+        serviceTarget1.Should().NotBe(serviceTarget2);
+    }
+
+    [Fact]
     public void ServiceTargetsShouldBeEqual()
     {
         var serviceTarget1 = ServiceTarget.FromEntity("light.kitchen");
         var serviceTarget2 = ServiceTarget.FromEntity("light.kitchen");
         serviceTarget1.Should().Be(serviceTarget2);
+    }
+
+    [Fact]
+    public void ServiceTargetsShouldNotBeEqual()
+    {
+        var serviceTarget1 = ServiceTarget.FromEntity("light.kitchen");
+        var serviceTarget2 = ServiceTarget.FromEntity("light.livingroom");
+        serviceTarget1.Should().NotBe(serviceTarget2);
     }
 
     [Fact]
@@ -55,12 +71,33 @@ public class ServiceTargetTest
     }
 
     [Fact]
+    public void ServiceTargetsOperatorNotEqualNorOverridden()
+    {
+        var serviceTarget1 = ServiceTarget.FromEntities("light.kitchen", "light.livingroom");
+        var serviceTarget2 = ServiceTarget.FromEntities("light.kitchen", "light.kitchen");
+
+        // The == operator is not overridden, so the == operator for ServiceTarget will only work on reference equality.  You will ALWAYS get true for not equal if not the same reference object
+        Assert.True((serviceTarget1 != serviceTarget2), "ServiceTargets should not be equal using !=");
+    }
+
+    [Fact]
     public void ServiceTargetsOperatorEqualReference()
     {
         var serviceTarget1 = ServiceTarget.FromEntities("light.kitchen", "light.livingroom");
         var serviceTarget1b = serviceTarget1;   // Reference to the same object
 
         Assert.True((serviceTarget1 == serviceTarget1b), "ServiceTargets references should be equal using ==");
+    }
+
+    [Fact]
+    public void ServiceTargetsOperatorNotEqualReference()
+    {
+        var serviceTarget1 = ServiceTarget.FromEntity("light.kitchen");
+        var serviceTarget2 = ServiceTarget.FromEntity("light.livingroom");
+        var serviceTarget1b = serviceTarget1;   // Reference to the same object
+        var serviceTarget2b = serviceTarget2;
+
+        Assert.True((serviceTarget1b != serviceTarget2b), "ServiceTargets references of different objects should not be equal using !=");
     }
 
     [Fact]
@@ -71,4 +108,14 @@ public class ServiceTargetTest
 
         Assert.True(serviceTarget1.Equals(serviceTarget2), "ServiceTargets should be equal using Equals.object");
     }
+
+    [Fact]
+    public void ServiceTargetObjectEqualShouldNotBeTrue()
+    {
+        var serviceTarget1 = ServiceTarget.FromEntity("light.kitchen");
+        object serviceTarget2 = ServiceTarget.FromEntity("light.livingroom");
+
+        Assert.False(serviceTarget1.Equals(serviceTarget2), "ServiceTargets should not be equal using Equals.object");
+    }
+
 }
