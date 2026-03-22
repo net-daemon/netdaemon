99b2af6acf6f51db6f22706ca31ca36d6a97442c
Duane McKibbin <duanemck@users.noreply.github.com>
2026-03-22 15:07:42 +0200

    Add sun events, i.e. scheduler.RunAtSunset() (#1358)
    Co-authored-by: Tomas Hellström <tomas.hellstrom@yahoo.se>

---
+++ src/Extensions/NetDaemon.Extensions.Scheduling/SunEvents/SolarCalendar.cs
@@ -0,0 +1,30 @@
+﻿using Innovative.SolarCalculator;
+
+namespace NetDaemon.Extensions.Scheduler.SunEvents;
+
+internal class SolarCalendar : ISolarCalendar
+{
+    private readonly SolarTimes _cachedCalculator;
+
+    public SolarCalendar(Coordinates coordinates)
+    {
+        _cachedCalculator = new SolarTimes(DateTime.Now, coordinates.Latitude, coordinates.Longitude);
+    }
+
+    private SolarTimes SolarCalculator
+    {
+        get
+        {
+            _cachedCalculator.ForDate = DateTime.Now;
+            return _cachedCalculator;
+        }
+    }
+
+    public DateTimeOffset Sunset => SolarCalculator.Sunset;
+
+    public DateTimeOffset Sunrise => SolarCalculator.Sunrise;
+
+    public DateTimeOffset Dusk => SolarCalculator.DuskCivil;
+
+    public DateTimeOffset Dawn => SolarCalculator.DawnCivil;
+}
---
+++ src/Extensions/NetDaemon.Extensions.Scheduling/SunEvents/SunEventScheduler.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+[assembly: InternalsVisibleTo("NetDaemon.Extensions.Scheduling.Tests")]
+
+namespace NetDaemon.Extensions.Scheduler.SunEvents;
+
+internal sealed class SunEventScheduler : ISunEventScheduler
+{
+    private readonly IScheduler _reactiveScheduler;
+    private readonly ISolarCalendar _solarCalendar;
+
+    public SunEventScheduler(ISolarCalendar solarCalendar, IScheduler reactiveScheduler)
+    {
+        _reactiveScheduler = reactiveScheduler;
+        _solarCalendar = solarCalendar;
+    }
+    
+    internal IDisposable RunAtSunEvent(Func<DateTimeOffset> getSunEventTime, Action action)
+    {
+        var todaysSunEvent = getSunEventTime().ToLocalTime();
+        var now = _reactiveScheduler.Now.ToLocalTime();
+        var tomorrow = now.Date.AddDays(1);
+
+        //Only schedule if the sun event is still going to occur today, the cron schedule will take over from tomorrow
+        if (todaysSunEvent > now && todaysSunEvent < tomorrow)
+        {
+            _reactiveScheduler.Schedule(todaysSunEvent, action);
+        }
+
+        return _reactiveScheduler.ScheduleCron("0 0 * * *", () =>
+        {
+            _reactiveScheduler.Schedule(getSunEventTime(), action);
+        });
+    }
+
+    /// <inheritdoc/>
+    public IDisposable RunAtSunset(Action action)
+    {
+        return RunAtSunEvent(() => _solarCalendar.Sunset, action);
+    }
+
+    /// <inheritdoc/>
+    public IDisposable RunAtDawn(Action action)
+    {
+        return RunAtSunEvent(() => _solarCalendar.Dawn, action);
+    }
+
+    /// <inheritdoc/>
+    public IDisposable RunAtSunrise(Action action)
+    {
+        return RunAtSunEvent(() => _solarCalendar.Sunrise, action);
+    }
+
+    /// <inheritdoc/>
+    public IDisposable RunAtDusk(Action action)
+    {
+        return RunAtSunEvent(() => _solarCalendar.Dusk, action);
+    }
+}
